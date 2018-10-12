6d4468a66876c0ad831b38c5c056e8f59f24f71a
lwong <fromosia@gmail.com>
2018-10-12 16:00:47 +0800

    Update codes

---
+++ Networks/Regional.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class StandardConv(nn.Module):
+    def __init__(self, inchan, outchan, kernsize=5, padding=False):
+        super(StandardConv, self).__init__()
+        if padding:
+            self.conv = nn.Conv3d(inchan, outchan, kernsize, padding=(kernsize - 1) /2)
+        else:
+            self.conv = nn.Conv3d(inchan, outchan, kernsize)
+        self.bn = nn.BatchNorm3d(outchan)
+
+    def forward(self, x):
+        return F.relu(self.bn(self.conv(x)))
+
+class StandardDeConv(nn.Module):
+    def __init__(self, inchan, outchan, kernsize=5):
+        super(StandardDeConv, self).__init__()
+        self.dconv = nn.ConvTranspose3d(inchan, outchan, kernsize)
+        self.bn = nn.BatchNorm3d(outchan)
+
+    def forward(self, x):
+        return self.bn(self.dconv(x))
+
+class StrideConv(nn.Module):
+    def __init__(self, inchan, outchan, kernsize=5, stride=2):
+        super(StrideConv, self).__init__()
+        self.dconv = nn.Conv3d(inchan, outchan, kernsize, stride=tuple(stride), padding=(kernsize - 1) /2)
+        self.bn = nn.BatchNorm3d(outchan)
+
+    def forward(self, x):
+        return self.bn(self.dconv(x))
+
+
+class Shallow(nn.Module):
+    def __init__(self):
+        super(Shallow, self).__init__()
+        self.conv1 = StandardConv(1, 16, 5)
+        self.conv2 = StandardConv(16, 32, 5)
+        self.conv3 = StandardConv(32, 64, 5)
+        self.conv4 = StandardDeConv(64, 32, 5)
+        self.conv5 = StandardDeConv(32, 16, 5)
+        self.conv6 = StandardDeConv(16, 1, 5)
+        self.initbn = nn.BatchNorm3d(1)
+
+    def forward(self, x):
+        x = self.initbn(x)
+        x = self.conv1(F.max_pool3d(x, kernel_size=[1, 2, 2]))
+        x = self.conv6(self.conv5(self.conv4(self.conv3(self.conv2(x)))))
+        x = F.upsample(x, scale_factor=[1, 2, 2], mode='trilinear')
+        return x
+
+
+class MultiScale(nn.Module):
+    def __init__(self):
+        super(MultiScale, self).__init__()
+        self.conv1 = StandardConv(1, 16, 5, padding=True)
+        self.poolConv1 = StrideConv(16, 32, 5, (1, 2, 2))
+        self.conv2 = StandardConv(32, 64, 5, padding=True)
+        self.conv3 = StandardConv(64, 128, 5, padding=True)
+        self.poolConv2 = StrideConv(128, 256, 3, (2, 2, 2))
+        self.conv4 = StandardConv(256, 512, 3, padding=True)
+        self.conv5 = StandardConv(512, 256, 3, padding=True)
+        self.conv6 = StandardConv(256, 128, 3, padding=True)
+
+        self.conv7 = StandardConv(128, 16, 3, padding=True)
+        self.conv8 = StandardConv(16, 1, 3, padding=True)
+
+        self.initbn = nn.BatchNorm3d(1)
+
+    def forward(self, x):
+        k = self.initbn(x)
+        k = self.conv1(F.max_pool3d(k, kernel_size=[1, 2, 2]))
+        k1 = self.conv3(self.conv2(self.poolConv1(k)))
+        k2 = self.conv6(self.conv5(self.conv4(self.poolConv2(k1))))
+        k1 = F.upsample(k2, scale_factor=2, mode='nearest') + k1
+        k = F.upsample(self.conv7(k1), scale_factor=[1, 2, 2], mode='trilinear') + k
+        x = F.upsample(self.conv8(k), scale_factor=[1, 2, 2], mode='trilinear')
+        # x = k - x
+        return x
+
+
+
+
+class ShallowMasked(nn.Module):
+    def __init__(self, indim):
+        super(ShallowMasked, self).__init__()
+
+        assert len(indim) == 2
+        d = [1, 1, 1]
+        d.extend(indim)
+        self.weightmask = nn.Parameter(torch.ones(d))
+        self.conv1 = StandardConv(1, 16, 5)
+        self.conv2 = StandardConv(16, 32, 5)
+        self.conv3 = StandardConv(32, 64, 5)
+        self.conv4 = StandardDeConv(64, 64, 5)
+        self.conv5 = StandardDeConv(64, 32, 5)
+        self.conv6 = StandardDeConv(32, 1, 5)
+        self.initbn = nn.BatchNorm3d(1)
+
+    def forward(self, x):
+        print torch.sum(self.weightmask).data[0]
+        x = x * self.weightmask.expand_as(x)
+        x = self.initbn(x)
+        x = self.conv1(F.max_pool3d(x, kernel_size=[1, 4, 4]))
+        x = self.conv6(self.conv5(self.conv4(self.conv3(self.conv2(x)))))
+        x = F.upsample(x, scale_factor=[1, 4, 4], mode='trilinear')
+        return x
+
---
+++ main_phase1.py
@@ -0,0 +1,213 @@
+import argparse
+import os
+import logging
+import numpy as np
+import datetime
+
+from tqdm import tqdm
+from MedImgDataset import ImageDataSet
+from torch.utils.data import DataLoader, TensorDataset, sampler
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+from tensorboardX import SummaryWriter
+from torchvision.utils import make_grid
+from Networks import MultiScale
+
+from Loss import normalized_loss
+from SimpleITK import WriteImage
+
+# import your own newtork
+
+def LogPrint(msg, level=20):
+    logging.getLogger(__name__).log(level, msg)
+    tqdm.write(msg)
+
+
+def main(a):
+    ##############################
+    # Error Check
+    #-----------------
+    mode = 0 # Training Mode
+    assert os.path.isdir(a.input), "Input data directory not exist!"
+    if a.train is None:
+        mode = 1 # Eval mode
+
+
+    ##############################
+    # Training Mode
+    if not mode:
+        assert os.path.isdir(a.train), "Ground truth directory cannot be opened!"
+        inputDataset= ImageDataSet(a.input, dtype=np.float32, verbose=True)
+        gtDataset   = ImageDataSet(a.train, dtype=np.float32, verbose=True)
+        trainingSet = TensorDataset(inputDataset, gtDataset)
+        loader      = DataLoader(trainingSet, batch_size=a.batchsize, shuffle=True, num_workers=4)
+                                 # sampler=sampler.WeightedRandomSampler(np.ones(len(trainingSet)).tolist(), a.batchsize*100))
+
+        writer = SummaryWriter('/media/storage/PytorchRuns/MCA')
+        # Load Checkpoint or create new network
+        #-----------------------------------------
+        net = MultiScale()
+        net = nn.DataParallel(net)
+        net.train(True)
+        if os.path.isfile(a.checkpoint):
+            assert os.path.isfile(a.checkpoint)
+            LogPrint("Loading checkpoint " + a.checkpoint)
+            net.load_state_dict(torch.load(a.checkpoint))
+
+        trainparams = {}
+        if not a.trainparams is None:
+            import ast
+            trainparams = ast.literal_eval(a.trainparams)
+
+        lr = trainparams['lr'] if trainparams.has_key('lr') else 1e-5
+        mm = trainparams['momentum'] if trainparams.has_key('momentum') else 0.01
+
+
+        criterion = nn.SmoothL1Loss()
+        optimizer = optim.SGD([{'params': net.parameters(),
+                                'lr': lr, 'momentum': mm}])
+        # optimizer = optim.ASGD(net.parameters(), lr, weight_decay=a.decay)
+        if a.usecuda:
+            criterion = criterion.cuda()
+            net = net.cuda()
+            # optimizer.cuda()
+
+        lastloss = 1e32
+        losses = []
+        for i in tqdm(range(a.epoch), desc='Epoch', leave=False):
+
+            E = []
+            for index, samples in enumerate(loader):
+                optimizer.zero_grad()
+
+                if a.usecuda:
+                    s = Variable(samples[0]).cuda()
+                    g = Variable(samples[1]).cuda()
+                else:
+                    s, g = Variable(samples[0]), Variable(samples[1])
+
+
+                out = net.forward(s.unsqueeze(1))
+                # out = normalized_loss.BatchNormLayer().forward(out)
+                g = normalized_loss.BatchNormLayer().forward(g)
+                loss = criterion(out.squeeze(), g.float())
+                loss.backward()
+                optimizer.step()
+                E.append(loss.data[0])
+                LogPrint("\t[Step %04d] Loss: %.010f"%(index, loss.data[0]))
+                if a.plot:
+                    try:
+                        # visualization.Visualize2D(out[0].squeeze().cpu().data * 1E4,
+                        #                           g[0].squeeze().cpu().data * 1E4,
+                        #                           env="MCA_run", nrow=3, indexrange=[50,60])
+
+                        poolim = make_grid(F.avg_pool2d(out[0].squeeze().unsqueeze(1), 4).data, nrow=4, padding=1, normalize=True)
+                        poolgt = make_grid(F.avg_pool2d(g[0].squeeze().unsqueeze(1), 4).data, nrow=4, padding=1, normalize=True)
+                        writer.add_image('Image/Image', poolim, index + i * len(loader))
+                        writer.add_image('Image/Groundtruth', poolgt, index + i * len(loader))
+                        writer.add_scalar('Loss', loss.data[0], index + i * len(loader))
+                        # del poolim, poolgt
+                    except AssertionError:
+                        tqdm.write(str(g[0].data.size()))
+
+            losses.append(E)
+            if np.array(E).mean() <= lastloss:
+                backuppath = "./Backup/checkpoint_Shallow.pt"
+                torch.save(net.state_dict(), backuppath)
+                lastloss = np.array(E).mean()
+            LogPrint("[Epoch %04d] Loss: %.010f"%(i, np.array(E).mean()))
+
+             # Decay learning rate
+            # if a.decay != 0 and i % 100 == 0:
+            #     for pg in optimizer.param_groups:
+            #         pg['lr'] = pg['lr'] * np.exp(-i * a.decay / float(a.epoch))
+
+
+    # Evaluation mode
+    else:
+        if not os.path.isdir(a.output):
+            os.mkdir(a.output)
+            if not os.path.isdir(a.output):
+                print "Cannot create output directories."
+                return
+
+        inputDataset= ImageDataSet(a.input, dtype=np.float32, verbose=True)
+        loader      = DataLoader(inputDataset, batch_size=a.batchsize, shuffle=False)
+
+        net = MultiScale()
+        net = nn.DataParallel(net)
+        if os.path.isfile(a.checkpoint):
+            LogPrint("Loading parameters " + a.checkpoint)
+            net.load_state_dict(torch.load(a.checkpoint))
+            net.train(False)
+        else:
+            LogPrint("Parameters file cannot be opened!")
+            return
+
+
+
+        if a.usecuda:
+            net = net.cuda()
+
+        for i, samples in enumerate(tqdm(loader)):
+            try:
+                s = Variable(samples, volatile=True)
+                if a.usecuda:
+                    s = s.cuda()
+                out = net.forward(s.unsqueeze(1))
+                out = ImageDataSet.WrapImageWithMetaData(out.data.cpu().squeeze().numpy(), inputDataset.metadata[i])
+
+                outname = a.output + "/" + os.path.basename(inputDataset.dataSourcePath[i])
+                tqdm.write("Writing " + outname)
+                WriteImage(out, outname)
+            except:
+                tqdm.write("Error occured for sample " + inputDataset.dataSourcePath[i])
+        pass
+
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Training reconstruction from less projections.")
+    parser.add_argument("input", metavar='input', action='store',
+                        help="Train/Target input", type=str)
+    parser.add_argument("-t", "--train", metavar='train', action='store', type=str, default=None,
+                        help="Required directory with target data which serve as ground truth for training. Do no" 
+                             "Set this to enable training mode.")
+    parser.add_argument("-o", metavar='output', dest='output', action='store', type=str, default=None,
+                        help="Set where to store outputs for eval mode")
+    parser.add_argument("-p", dest='plot', action='store_true', default=False,
+                        help="Select whether to disply the plot for stepwise loss")
+    parser.add_argument("-d", "--decayLR", dest='decay', action='store', type=float, default=0,
+                        help="Set decay halflife of the learning rates.")
+    parser.add_argument("-e", "--epoch", dest='epoch', action='store', type=int, default=0,
+                        help="Select network epoch.")
+    parser.add_argument("-s", "--steps", dest='steps', action='store', type=int, default=1000,
+                        help="Specify how many steps to run per epoch.")
+    parser.add_argument("-b", "--batchsize", dest='batchsize', action='store', type=int, default=5,
+                        help="Specify batchsize in each iteration.")
+    parser.add_argument("--load", dest='checkpoint', action='store', default='',
+                        help="Specify network checkpoint.")
+    parser.add_argument("--useCUDA", dest='usecuda', action='store_true',default=False,
+                        help="Set whether to use CUDA or not.")
+    parser.add_argument("--train-params", dest='trainparams', action='store', type=str, default=None,
+                        help="Path to a file with dictionary of training parameters written inside")
+    parser.add_argument("--log", dest='log', action='store', type=str, default=None,
+                        help="If specified, all the messages will be written to the specified file.")
+    parser.add_argument("--stage", dest='stage', default=1, action='store', type=int,
+                        help="Stage 1: Feature location, Stage2: TOCI classification")
+    a = parser.parse_args()
+
+    if a.log is None:
+        if not os.path.isdir("./Backup/Log"):
+            os.mkdir("./Backup/Log")
+        if a.train:
+            a.log = "./Backup/Log/run_%s.log"%(datetime.datetime.now().strftime("%Y%m%d"))
+        else:
+            a.log = "./Backup/Log/eval_%s.log"%(datetime.datetime.now().strftime("%Y%m%d"))
+
+    logging.basicConfig(format="[%(asctime)-12s - %(levelname)s] %(message)s", filename=a.log, level=20)
+
+    main(a)
