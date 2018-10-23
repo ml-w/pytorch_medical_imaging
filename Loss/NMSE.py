7444157a0809781e5b7d2541717a190c87084342
Matthew Wong <fromosia@gmail.com>
2018-10-23 19:45:57 +0800

    NMSE

---
+++ Loss/NMSE.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class NMSELoss(nn.Module):
+    def __init__(self, size_average=True, reduce=True):
+        super(NMSELoss, self).__init__()
+        self._size_average=size_average
+        self._reduce = reduce
+
+    def forward(self, input, target):
+        mseloss = F.mse_loss(target, input,
+                             size_average = self._size_average,
+                             reduce = self._reduce)
+        zeros = Variable(torch.zeros(input.size()), requires_grad=False)
+        if self.cuda():
+            zeros = zeros.cuda()
+        factor = F.mse_loss(input, zeros,
+                            size_average=self._size_average,
+                            reduce = self._reduce)
+        return mseloss / factor
+
