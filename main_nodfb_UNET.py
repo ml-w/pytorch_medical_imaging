2158aa32744e1d7e7434a5a2a17038e64c208f76
Matthew Wong <fromosia@gmail.com>
2018-10-23 11:46:47 +0800

    Implement output mode

--- main_nodfb_UNET.py
+++ main_nodfb_UNET.py
@@ -4,7 +4,7 @@ import logging
 import numpy as np
 import datetime
 
-from MedImgDataset import ImageDataSet
+from MedImgDataset import ImageDataSet, ImagePatchesLoader
 from torch.utils.data import DataLoader, TensorDataset
 from torch.autograd import Variable
 from tqdm import tqdm
@@ -47,32 +47,39 @@ def main(a):
     if not mode:
         if a.loadbyfilelist is None:
             inputDataset= ImageDataSet(a.input, dtype=np.float32, verbose=True, debugmode=False, filesuffix=a.lsuffix,
-                                   loadBySlices=0)
+                                       loadBySlices=0)
             gtDataset   = ImageDataSet(a.train, dtype=np.float32, verbose=True, debugmode=False, loadBySlices=0)
         else:
             gt_filelist, input_filelist = a.loadbyfilelist.split(',')
             inputDataset= ImageDataSet(a.input, dtype=np.float32, verbose=True, loadBySlices=0, filelist=input_filelist,
-                                   filesuffix=a.lsuffix, debugmode=False)
+                                       filesuffix=a.lsuffix, debugmode=False)
             gtDataset   = ImageDataSet(a.train, dtype=np.float32, verbose=True, loadBySlices=0, filelist=gt_filelist,
-                                   debugmode=False)
+                                       debugmode=False)
 
+        inputDataset = ImagePatchesLoader(inputDataset, patch_size=256, patch_stride=128)
+        gtDataset = ImagePatchesLoader(gtDataset, 256, 128, reference_dataset=inputDataset)
+
+        print inputDataset.size(), gtDataset.size()
         trainingSet = TensorDataset(inputDataset, gtDataset)
         loader      = DataLoader(trainingSet, batch_size=a.batchsize, shuffle=True, num_workers=4)
         # sampler=sampler.WeightedRandomSampler(np.ones(len(trainingSet)).tolist(), a.batchsize*100))
 
-        tensorboard_rootdir = os.environ['TENSORBOARD_LOGDIR']
+        try:
+            tensorboard_rootdir = os.environ['TENSORBOARD_LOGDIR']
+        except:
+            tensorboard_rootdir = "/media/storage/PytorchRuns"
         try:
             if not os.path.isdir(tensorboard_rootdir):
                 print "Cannot read from TENORBOARD_LOGDIR, retreating to default path..."
                 tensorboard_rootdir = "/media/storage/PytorchRuns"
-            writer = SummaryWriter(tensorboard_rootdir + "/DFB_%s_"%a.lsuffix+datetime.datetime.now().strftime("%Y%m%d_%H%M"))
+            writer = SummaryWriter(tensorboard_rootdir + "/DirectUNET_%s_"%a.lsuffix+datetime.datetime.now().strftime("%Y%m%d_%H%M"))
         except OSError:
             writer = None
             a.plot = False
 
         # Load Checkpoint or create new network
         #-----------------------------------------
-        net = UNet(1)
+        net = UNet(1, False)
         # net = nn.DataParallel(net)
         net.train(True)
         if os.path.isfile(a.checkpoint):
@@ -159,8 +166,8 @@ def main(a):
 
     # Evaluation mode
     else:
-        inputDataset= Subbands(a.input, dtype=np.float32, verbose=True,
-                               debugmode=False, filesuffix=a.lsuffix, loadBySlices=0, filelist=a.loadbyfilelist)
+        inputDataset= ImageDataSet(a.input, dtype=np.float32, verbose=True, filesuffix=a.lsuffix,
+                                   debugmode=False, loadBySlices=0, filelist=a.loadbyfilelist)
         loader = DataLoader(inputDataset, batch_size=a.batchsize, shuffle=False, num_workers=4)
         print inputDataset
 
@@ -173,7 +180,7 @@ def main(a):
 
         # Load Checkpoint or create new network
         #-----------------------------------------
-        net = UNet(8)
+        net = UNet(1, False)
         net.load_state_dict(torch.load(a.checkpoint))
         net.train(False)
         if a.usecuda:
@@ -185,10 +192,10 @@ def main(a):
                 s = Variable(samples, volatile=True).float().cuda()
             else:
                 s = Variable(samples, volatile=True).float()
-
-            out = net.forward(s.permute(0, 3, 1, 2)).squeeze()
-            out_tensor.append(out.data.permute(0, 2, 3, 1).cpu())
+            out = net.forward(s.unsqueeze(1)).squeeze()
+            out_tensor.append(out.cpu().data)
         out_tensor = torch.cat(out_tensor, dim=0)
+        print out_tensor.size()
         inputDataset.Write(out_tensor, a.output)
         pass
 
