b0fedc931a789c11b5795f41a990d2b33ad9f312
Matthew Wong <fromosia@gmail.com>
2018-10-26 02:46:20 +0800

    Fixed Haar for 2D pooling

--- Networks/TightFrameUNet.py
+++ Networks/TightFrameUNet.py
@@ -27,33 +27,52 @@ class DoubleConv(nn.Module):
 class HaarDown(nn.Module):
     def __init__(self, inchan):
         super(HaarDown, self).__init__()
-        self.haar_matrix = torch.tensor([[1., -1.], [-1., 1.]]).float() / 2.
-        self.weights = torch.zeros([inchan, inchan, 2, 2])
+        self.haar_matrix = torch.tensor([[1., -1., -1., 1.],
+                                         [1., -1., 1., -1.],
+                                         [1., 1., -1., -1.]]).float().view(3, 2, 2) / 2.
+        self.weights = torch.zeros([inchan*3, inchan, 2, 2])
         for i in xrange(inchan):
-            self.weights[i, i] = self.haar_matrix
+            self.weights[3*i:3*i+3, i] = self.haar_matrix
 
     def forward(self, x):
         if x.is_cuda:
-            x = F.conv2d(x, self.weights.cuda())
+            x = F.conv2d(x, self.weights.cuda(), stride=2)
         else:
-            x = F.conv2d(x, self.weights)
+            x = F.conv2d(x, self.weights, stride=2)
         return x
 
 
 class HaarUp(nn.Module):
     def __init__(self, inchan):
         super(HaarUp, self).__init__()
-        self.haar_matrix = torch.tensor([[1., -1.],[-1., 1.]]).float() / 2.
-        self.weights = torch.zeros([inchan, inchan, 2, 2])
+        self.haar_matrix = torch.tensor([[1., -1., -1., 1.],
+                                         [1., -1., 1., -1.],
+                                         [1., 1., -1., -1.]]).float().view(3, 2, 2) / 2.
+        self.weights = torch.zeros([inchan*3, inchan, 2, 2])
         for i in xrange(inchan):
-            self.weights[i, i] = self.haar_matrix
+            self.weights[3*i:3*i+3, i] = self.haar_matrix
 
     def forward(self, x):
         if x.is_cuda:
-            x = F.conv_transpose2d(x, self.weights.cuda())
+            x = F.conv_transpose2d(x, self.weights.cuda(), stride=2)
         else:
-            x = F.conv_transpose2d(x, self.weights)
+            x = F.conv_transpose2d(x, self.weights, stride=2)
         return x
+
+
+class AvgUp(nn.Module):
+    def __init__(self, upscale):
+        super(AvgUp, self).__init__()
+        self.upscale = upscale
+
+    def forward(self, x):
+        s = x.size(1)
+        repeat_idx = [1] * x.dim()
+        repeat_idx[1] = self.upscale**2
+        a = x.repeat(*(repeat_idx))
+        order_index = torch.cat([s * torch.arange(self.upscale**2) + i for i in range(s)]).to(torch.long)
+        x = torch.index_select(a, 1, order_index)
+        return F.pixel_shuffle(x, upscale_factor=self.upscale)
 
 
 class Haar(nn.Module):
@@ -63,6 +82,7 @@ class Haar(nn.Module):
             HaarDown(inchan),
             HaarUp(inchan)
         )
+
 
     def forward(self, x):
         x = self.haar(x)
@@ -94,10 +114,10 @@ class Up(nn.Module):
 
     def forward(self, x1, x2, x3):
         x1 = self.up(x1)
-        diffX2 = x1.size()[2] - x3.size()[2]
-        diffY2 = x1.size()[3] - x3.size()[3]
-        diffX1 = x1.size()[2] - x2.size()[2]
-        diffY1 = x1.size()[3] - x2.size()[3]
+        diffY2 = abs(x1.size()[2] - x3.size()[2])
+        diffX2 = abs(x1.size()[3] - x3.size()[3])
+        diffY1 = abs(x1.size()[2] - x2.size()[2])
+        diffX1 = abs(x1.size()[3] - x2.size()[3])
         x2 = F.pad(x2, (diffX1 // 2, int(diffX1 / 2),
                         diffY1 // 2, int(diffY1 / 2)))
         x3 = F.pad(x3, (diffX2 // 2, int(diffX2 / 2),
@@ -119,7 +139,7 @@ class OutConv(nn.Module):
 
 
 class TightFrameUNet(nn.Module):
-    def __init__(self, chan):
+    def __init__(self, chan, residual=False):
         super(TightFrameUNet, self).__init__()
         self.inc = DoubleConv(chan, 64)
         self.outc = OutConv(64, chan)
@@ -131,12 +151,15 @@ class TightFrameUNet(nn.Module):
         self.haar2 = Haar(128)
         self.haar3 = Haar(256)
         self.haar4 = Haar(512)
+        self.haar5 = Haar(512)
         self.up1 = Up(1536, 256)
         self.up2 = Up(1024, 128)
         self.up3 = Up(512, 64)
         self.up4 = Up(256, 64)
+        self.residual = residual
 
     def forward(self, x):
+        temp = x
         x1 = self.inc(x)            # B x 64  x H x W
         x2 = self.down1(x1)         # B x 128 x H x W
         x3 = self.down2(x2)         # B x 256 x H x W
@@ -146,13 +169,16 @@ class TightFrameUNet(nn.Module):
         x2_haar = self.haar2(x2)
         x3_haar = self.haar3(x3)
         x4_haar = self.haar4(x4)
-        x5_haar = self.haar4(x5)
+        x5_haar = self.haar5(x5)
 
-        x = self.up1(x5_haar, x5, x4)
-        x = self.up2(x4_haar, x, x3 )
-        x = self.up3(x3_haar, x, x2)
-        x = self.up4(x2_haar, x, x1)
-        x = self.outc(x1_haar, x)
+        x = self.up1(x5, x5_haar, x4)
+        x = self.up2(x, x4_haar, x3 )
+        x = self.up3(x, x3_haar, x2)
+        x = self.up4(x, x2_haar, x1)
+        if self.residual:
+            x = self.outc(x1_haar, x) + temp
+        else:
+            x = self.outc(x1_haar, x)
         return x
 
 
