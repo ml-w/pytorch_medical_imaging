32007a5a8dee850a2819f2056eeea4b0b6cafc1f
Matthew Wong <fromosia@gmail.com>
2018-12-14 14:17:15 +0800

    Update attention networks

--- Networks/AttentionUNet.py
+++ Networks/AttentionUNet.py
@@ -21,7 +21,10 @@ class AttentionUNet(nn.Module):
         self.up4 = up(128, 64, True)
         self.outc = outconv(64, n_channels) if outchan is None else outconv(64, outchan)
 
-        self.gating = AttentionGating(512, 256)
+        self.gating1 = AttentionGating(512, 256)
+        self.gating2 = AttentionGating(512, 256)
+        self.gating3 = AttentionGating(256, 256)
+        self.gating4 = AttentionGating(128, 256)
 
         self.att1 = AttentionBlock(256, 256)
         self.att2 = AttentionBlock(128, 256)
@@ -35,19 +38,22 @@ class AttentionUNet(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
 
-        g = self.gating(x4)
+        g1 = self.gating1(x5)
+        g2 = self.gating2(x4)
+        g3 = self.gating3(x3)
+        g4 = self.gating4(x2)
 
         u1 = self.up1(x5, x4)
-        u1, att1 = self.att1(u1, g)
+        u1, att1 = self.att1(u1, g1)
 
         u2 = self.up2(u1, x3)
-        u2, att2 = self.att2(u2, g)
+        u2, att2 = self.att2(u2, g2)
 
         u3 = self.up3(u2, x2)
-        u3, att3 = self.att3(u3, g)
+        u3, att3 = self.att3(u3, g3)
 
         u4 = self.up4(u3, x1)
-        u4, att4 = self.att4(u4, g)
+        u4, att4 = self.att4(u4, g4)
 
         u5 = self.outc(u4)
 
@@ -61,4 +67,63 @@ class AttentionUNet(nn.Module):
 
     def get_att_map(self):
         return self.att
+
+
+class AttentionUNetPosAware(AttentionUNet):
+    def __init__(self, *args, **kwargs):
+        super(AttentionUNetPosAware, self).__init__(*args, **kwargs)
+
+        # self.fc = nn.Sequential(
+        #     nn.Conv1d(1, 128, 1),
+        #     nn.Conv1d(128, 256, 1),
+        #     nn.Conv1d(256, 128, 1),
+        #     nn.Conv1d(128, 64, 1),
+        #     nn.Conv1d(64, 2, 1)
+        # )
+        self.fc = nn.Sequential(
+            nn.Linear(1, 256),
+            nn.Linear(256, 256),
+            nn.Linear(256, 2)
+        )
+        self.outc = nn.Conv2d(64, 2, 1)
+
+    def forward(self, x, pos):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        g1 = self.gating1(x5)
+        g2 = self.gating2(x4)
+        g3 = self.gating3(x3)
+        g4 = self.gating4(x2)
+
+        x = self.up1(x5, x4)
+        x, att1 = self.att1(x, g1)
+
+        x = self.up2(x, x3)
+        x, att2 = self.att2(x, g2)
+
+        x = self.up3(x, x2)
+        x, att3 = self.att3(x, g3)
+
+        x = self.up4(x, x1)
+        x, att4 = self.att4(x, g4)
+
+        x = self.outc(x)
+
+        # expand pos
+        pos = self.fc(pos.view(len(pos), 1))
+        pos = pos.expand_as(x.permute(2, 3, 0, 1)).permute(2, 3, 0, 1)
+        x = x * pos
+
+        if not self.attmap:
+            del att1, att2, att3, att4
+            self.att = None
+            return x
+        else:
+            self.att = [att1, att2, att3, att4]
+            return x
+
 
--- Networks/__init__.py
+++ Networks/__init__.py
@@ -2,5 +2,7 @@ from DenseUNet import *
 from UNet import *
 from TightFrameUNet import *
 from AttentionUNet import *
+from AttentionDenseUNet import *
 
-__all__ = ['UNet', 'TightFrameUNet', 'DenseUNet2D', 'AttentionUNet', 'UNetPosAware']
+__all__ = ['UNet', 'TightFrameUNet', 'DenseUNet2D', 'AttentionUNet', 'UNetPosAware',
+           'AttentionUNetPosAware', 'AttentionDenseUNet2D']
