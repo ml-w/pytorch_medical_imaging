9bf0428535b97794cb5a93e453027704b59253f5
Matthew Wong <fromosia@gmail.com>
2019-03-11 13:30:17 +0800

    Add test case for LNDP and fix error for Multi-Channel texture.

--- Test/Test_ImageDataSetMultiChannel.py
+++ Test/Test_ImageDataSetMultiChannel.py
@@ -1,16 +1,23 @@
-from MedImgDataset import ImageDataSet, ImageDataSetMultiChannel, ImageDataSetAugment, \
-    ImageDataSetWithPos
+from MedImgDataset import ImageDataSet, ImageDataSetMultiChannel, ImageDataSetAugment, ImagePatchesLoader
+from MedImgDataset.Computation import lndp, lbp, ImageDataSetFilter
+import torch
 
 def main():
-    imset = ImageDataSetAugment('../NPC_Segmentation/01.NPC_dx', verbose=True, debugmode=True,
-                                loadBySlices=0, filesuffix="*T1*C*")
-    imt2set = ImageDataSetAugment('../NPC_Segmentation/01.NPC_dx', verbose=True, debugmode=True,
-                                loadBySlices=0, filesuffix="*T2*Reg*")
+    LBP = lambda x: torch.tensor(lbp(x.data.squeeze().numpy().astype('float')))
+    LNDP = lambda x: torch.tensor(lndp(x.data.squeeze().numpy().astype('float')))
+
+    imset = ImageDataSetFilter('../NPC_Segmentation/01.NPC_dx', verbose=True, debugmode=True,
+                                loadBySlices=0, filesuffix="*T1*C*", dtype=int, filter=LNDP)
+    imt2set = ImageDataSetFilter('../NPC_Segmentation/01.NPC_dx', verbose=True, debugmode=True,
+                                loadBySlices=0, filesuffix="*T1*C*", dtype=int, filter=LBP)
+    imset.as_type(torch.uint8)
+    imt2set.as_type(torch.uint8)
 
     test = ImageDataSetMultiChannel(imset, imt2set)
-    test = ImageDataSetWithPos(test)
+    test = ImagePatchesLoader(test, 128, 32)
 
-    print test[0:50][0].shape, test[0:50][1].shape
+    # print test[0][0].shape
+    print test[0].shape
 
 
 if __name__ == '__main__':
