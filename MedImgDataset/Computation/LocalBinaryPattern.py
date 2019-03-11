ea81f6d48ce67dfaa9b205c8f81ef32953396094
Matthew Wong <fromosia@gmail.com>
2019-03-11 16:14:10 +0800

    Add multi-processing for multi-dimensional input. Useless currently.

--- MedImgDataset/Computation/LocalBinaryPattern.py
+++ MedImgDataset/Computation/LocalBinaryPattern.py
@@ -1,10 +1,19 @@
 import numpy as np
+import multiprocessing as mpi
 from skimage.feature import local_binary_pattern
-
+from functools import partial
 
 def lbp(data, window=1):
     assert isinstance(data, np.ndarray), "Only support numpy arrays."
 
-    bn = local_binary_pattern(data, 8, window).astype('uint8')
-    bn = np.invert(bn)
+    if data.ndim == 2:
+        bn = local_binary_pattern(data, 8, window).astype('uint8')
+        bn = np.invert(bn)
+    else:
+        cores = mpi.cpu_count()
+        pool = mpi.Pool(cores)
+        pool_outputs = pool.map(partial(lbp, window=window), [data[i] for i in range(data.shape[0])])
+        pool.close()
+        pool.join()
+        bn = np.stack(pool_outputs, axis=0)
     return bn.astype('float')
--- MedImgDataset/Computation/LocalNeighborhoodDifferencePattern.py
+++ MedImgDataset/Computation/LocalNeighborhoodDifferencePattern.py
@@ -1,5 +1,7 @@
 from _LocalNeighborhoodDifferencePattern import LNDP
+import multiprocessing as mpi
 import numpy as np
+from functools import partial
 
 def lndp(data, window=1):
     assert isinstance(data, np.ndarray), "Only support numpy arrays."
@@ -9,7 +11,12 @@ def lndp(data, window=1):
         bn = LNDP(data, 8, window).astype('uint8')
         bn = np.invert(bn)
     else:
-        d = [LNDP(np.ascontiguousarray(data[i]), 8, window).astype('uint8')
-                for i in xrange(data.shape[0])]
-        bn = np.stack(d, axis=0)
+        cores = mpi.cpu_count()
+        pool = mpi.Pool(cores)
+        pool_outputs = pool.map(partial(lndp, window=window), [data[i] for i in range(data.shape[0])])
+        pool.close()
+        pool.join()
+        # d = [LNDP(np.ascontiguousarray(data[i]), 8, window).astype('uint8')
+        #         for i in xrange(data.shape[0])]
+        bn = np.stack(pool_outputs, axis=0)
     return bn.astype('float')
