4d439155586f71b47f3d5bad249ee77faebeaa7d
Matthew Wong <fromosia@gmail.com>
2019-12-26 18:14:13 +0800

    Change to display contours instead.

--- tb_plotter.py
+++ tb_plotter.py
@@ -1,7 +1,8 @@
 import torch
 import gc
 from logger import Logger
-from torchvision.utils import make_grid
+# from torchvision.utils import make_grid
+from Algorithms.visualization import draw_grid
 
 __all__ = ['TB_plotter']
 
@@ -30,34 +31,13 @@ class TB_plotter(object):
 
             val, ar = torch.max(out, 1)
             ss = img[0] if isinstance(img, list) else img
-            poolim = make_grid(ss[:Zrange].data.cpu(), nrow=nrow, padding=1, normalize=True)
-            poolgt = make_grid(gt[:Zrange].float().data.cpu(), nrow=nrow, padding=1, normalize=True)
-            poolseg = make_grid(ar[:Zrange].unsqueeze(1).float().data.cpu(), nrow=nrow, padding=1, range=[0, 1])
-            # make it red
-            poolseg[1] = 0
-            poolseg[2] = 0
 
-            # make it green
-            poolgt[0] = 0
-            poolgt[2] = 0
-
-            # alpha addition for overlaying
-            alpha_value = 0.5
-            alpha_map_seg = torch.zeros_like(poolseg)
-            alpha_map_seg[poolseg != 0] = alpha_value
-            alpha_map_gt = torch.zeros_like(poolgt)
-            alpha_map_gt[poolgt != 0] = alpha_value
-            poolseg = (poolim + poolseg * alpha_map_seg * (-alpha_map_seg + 1.)) / \
-                      (1. + alpha_map_seg * (-alpha_map_seg + 1.))
-
-            poolgt = (poolim + poolgt * alpha_map_gt * (-alpha_map_gt + 1.)) / \
-                      (1. + alpha_map_gt * (-alpha_map_gt + 1.))
-            self._writer.add_image('Image/Segmentation', poolseg, writer_index)
-            self._writer.add_image('Image/Groundtruth', poolgt, writer_index)
-            self._writer.add_image('Image/Image', poolim, writer_index)
+            grid = draw_grid(ss[:Zrange].cpu(), ar[:Zrange].cpu(), ground_truth=gt[:Zrange].cpu(), thickness=2)
+            self._writer.add_image('Image/Image', grid.transpose(2, 0, 1), writer_index)
 
 
-            del poolim, poolgt, poolseg, val, ar, alpha_map_gt, alpha_map_seg
+            # del poolim, poolgt, poolseg, val, ar,
+            del grid
             gc.collect()
         except Exception as e:
             import traceback, sys
