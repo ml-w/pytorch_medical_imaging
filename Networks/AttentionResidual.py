GradCAM improvements - update gradCAM funcitonalities - add slicewise normalization for GradCAM - fix forward hook problem

Networks/AttentionResidual.py
@@ -121,7 +121,7 @@ class AttentionResidualNet(nn.Module):
         x_w = F.avg_pool3d(x_w,kernel_size=x_w.shape[-3:]).squeeze()
         x_w = F.sigmoid(x_w) + 0.5
         if self.save_weight:
-            self.x_w = torch.tensor(x_w.data.cpu())
+            self.x_w = x_w.data.cpu()
         x_w = x_w.view([-1])
 
         # Permute the axial dimension to the last

Networks/GradCAM/FeatureExtractor.py
@@ -4,32 +4,56 @@ from torch.autograd import Variable
 class FeatureExtractor(object):
     """ Class for extracting activations and
     registering gradients from targetted intermediate layers """
-    def __init__(self, model, target_layers):
-        self.model = model
+    def __init__(self, model, target_layers, threeD=True):
+        self.outer = model
+        if isinstance(model, torch.nn.DataParallel):
+            self.model = model.module
+        else:
+            self.model = model
+        self.threeD = threeD
         self.target_layers = target_layers
         self.gradients = []
         self.features = []
+
         modnames = []
         mods = []
         for name, module in self.model.named_modules():
-            if name.find('.') == -1:
-                modnames.append(name)
-                mods.append(module)
+            # if name.find('.') == -1:
+            modnames.append(name)
+            mods.append(module)
 
         for name, module in self.model.named_modules():
             if name in self.target_layers:
                 module.register_backward_hook(self.save_gradient)
-                # module.register_forward_hook(self.save_features)
-                nextmod = mods[modnames.index(name) + 1]
-                nextmod.register_forward_hook(self.save_features)
+                module.register_forward_hook(self.save_features)
+                # nextmod = mods[modnames.index(name) + 1]
+                # nextmod.register_forward_hook(self.save_features)
 
-    def save_gradient(self, mod, grad_in, grad_out):
+    def save_gradient(self, mod, grad_in, grad_out, name=None):
+        # this is called n-times if they are put in multiple GPUs to compute
+        # if not mod in self.gradients:
+        #     self.gradients= {mod: [*grad_out]}
+        # else:
+        #     self.gradients[mod].append(*grad_out)
+        print("Grad size: {}".format([g.shape for g in grad_out]))
         self.gradients.append(grad_out)
 
-    def save_features(self, mod, input, output):
+    def save_features(self, mod, input, output, name=None):
         # if not isinstance(output, tuple):
         #     output = (output)
-        self.features.append(input)
+        # if not mod in self.features:
+        #     self.features = {mod: [*input]}
+        # else:
+        #     self.features[mod].append(*input)
+        # self.features.append(input)
+
+        print("Output size : {}".format([a.shape for a in output]))
+        if len(output) > 1:
+            self.features.append([torch.stack([a for a in output])])
+        else:
+            self.features.append(output)
+
 
     def __call__(self, x):
-        return self.model(x)
+        out = self.outer(x)
+        return out

main.py
@@ -279,7 +279,7 @@ def main(a, config, logger):
                 backuppath = "./Backup/cp_%s_%s.pt"%(net_datatype, net_nettype) \
                     if checkpoint_save is None else checkpoint_save
                 torch.save(solver.get_net().module.state_dict(), backuppath)
-                lastloss = np.array(E).mean()
+                lastloss = measure_loss
 
 
             try:
@@ -316,7 +316,7 @@ def main(a, config, logger):
                                  bool_usecuda, logger)
 
         if write_mode == 'GradCAM':
-            inferencer.grad_cam_write_out(['att3'])
+            inferencer.grad_cam_write_out(['att2'])
         else:
             with torch.no_grad():
                 inferencer.write_out()