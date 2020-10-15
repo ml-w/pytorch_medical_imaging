13e72867474456b3c7a86afe42359823d25c3907
Matthew Wong <fromosia@gmail.com>
2020-10-15 20:24:10 +0800

    Add CNN-GRU

--- pytorch_med_imaging/Networks/__init__.py
+++ pytorch_med_imaging/Networks/__init__.py
@@ -6,9 +6,10 @@ from .AttentionUNet import *
 from .AttentionDenseUNet import *
 from .LLinNet import *
 from .AttentionResidual import *
+from .CnnRnn import *
 
 __all__ = ['UNet', 'TightFrameUNet', 'DenseUNet2D', 'AttentionUNet', 'UNetPosAware',
            'AttentionUNetPosAware', 'AttentionDenseUNet2D', 'AttentionUNetLocTexAware',
            'UNetLocTexAware', 'UNetLocTexHist', 'UNetLocTexHistDeeper', 'LLinNet',
            'AttentionResidualNet', 'UNet_p', 'AttentionResidualNet_64', 'AttentionResidualNet_SW',
-           'AttentionResidualGRUNet']
+           'AttentionResidualGRUNet', 'CNNGRU']
