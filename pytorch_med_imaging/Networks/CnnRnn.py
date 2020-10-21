a5bb753667e990270feaae16799827b08b18a52c
Matthew Wong <fromosia@gmail.com>
2020-10-21 19:14:52 +0800

    Add more docs

--- pytorch_med_imaging/Networks/CnnRnn.py
+++ pytorch_med_imaging/Networks/CnnRnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from .Layers import DoubleConv3d, BGRUCell, BGRUStack
 
 class CNNGRU(nn.Module):
-    """
+    r"""
     3D CNN-GRU implemented following [1]_, with a bit of modifications including:
     - conv kernel size
     - add dropouts
@@ -24,7 +24,7 @@ class CNNGRU(nn.Module):
         embeding_size (list or tuple, Optional):
             Size of CNN decoded feature patch of the final CNN layer, which output will be pooled using
             `nn.AdaptivePooling3d` into a feature with fixed size specified by this argument and that
-            flattened before moving on into the GRU phase. Defaul to `(5,5)` (25-element vector)
+            flattened before moving on into the GRU phase. Defaul to `(20,5,5)` (25-element vector per slice)
         dropout (float, Optional):
             Dropout for CNN decoding layers. Default to 0.2.
 
@@ -35,39 +35,48 @@ class CNNGRU(nn.Module):
         >>> from pytorch_model_summary import *
         >>> import torch
         >>> net = CNNGRU(1, 3)
-        >>> in_tensor = torch.rand(size=[1, 1, 22, 444, 444])
+        >>> in_tensor = torch.rand(size=[1, 1, 20, 444, 444])
         >>> net = net.cuda()
         >>> in_tensor = in_tensor.cuda()
         >>> with torch.no_grad():
         >>>     summary(net, in_tensor, print_summary=True)
 
 
-        ----------------------------------------------------------------------------------
-        Layer (type)               Output Shape         Param #     Tr. Param #
-        ==================================================================================
-        DoubleConv3d-1      [1, 32, 22, 444, 444]          28,704          28,704
-        DoubleConv3d-2      [1, 64, 22, 222, 222]          55,680          55,680
-        DoubleConv3d-3     [1, 128, 22, 111, 111]         221,952         221,952
-        DoubleConv3d-4       [1, 256, 22, 56, 56]         886,272         886,272
-        DoubleConv3d-5       [1, 512, 22, 28, 28]       3,542,016       3,542,016
-        AdaptiveMaxPool3d-6    [1, 512, 20, 5, 5]               0               0
-        BGRUStack-7               [1, 20, 512, 6]         276,480         276,480
-        BGRUCell-8         [1, 512, 6], [2, 1, 3]           2,250           2,250
-        Linear-9                        [1, 3, 1]             513             513
-        ==================================================================================
+        +--------------------+------------------------+---------------+---------------+
+        |Layer (type)        |       Output Shape     |        Param #|    Tr. Param #|
+        +====================+========================+===============+===============+
+        |DoubleConv3d-1      |  [1, 32, 22, 444, 444] |         28,704|        28,704 |
+        +--------------------+------------------------+---------------+---------------+
+        |DoubleConv3d-2      |  [1, 64, 22, 222, 222] |         55,680|        55,680 |
+        +--------------------+------------------------+---------------+---------------+
+        |DoubleConv3d-3      | [1, 128, 22, 111, 111] |        221,952|       221,952 |
+        +--------------------+------------------------+---------------+---------------+
+        |DoubleConv3d-4      |   [1, 256, 22, 56, 56] |        886,272|       886,272 |
+        +--------------------+------------------------+---------------+---------------+
+        |DoubleConv3d-5      |   [1, 512, 22, 28, 28] |      3,542,016|     3,542,016 |
+        +--------------------+------------------------+---------------+---------------+
+        |AdaptiveMaxPool3d-6 |     [1, 512, 20, 5, 5] |              0|             0 |
+        +--------------------+------------------------+---------------+---------------+
+        |BGRUStack-7         |        [1, 20, 512, 6] |        276,480|       276,480 |
+        +--------------------+------------------------+---------------+---------------+
+        |BGRUCell-8          |            [1, 512, 6] |          2,250|         2,250 |
+        +--------------------+------------------------+---------------+---------------+
+        |Linear-9            |              [1, 3, 1] |            513|           513 |
+        +--------------------+------------------------+---------------+---------------+
+
+    References:
+    (TODO: Add reference to CNNGRU)
+
     """
     def __init__(self,
                  in_ch: int,
                  out_ch: int,
                  first_conv_out_ch: int = 32,
                  decode_layers: int = 3,
-                 embeding_size: tuple = (5, 5),
+                 embedding_size: tuple = (20, 5, 5),
+                 gru_layers: int = 1,
                  dropout: float = 0.2
-         ):
-        """
-
-
-        """
+                 ):
         super(CNNGRU, self).__init__()
 
         self._config = {
@@ -75,7 +84,7 @@ class CNNGRU(nn.Module):
             'out_ch': torch.Tensor([out_ch]),
             'first_conv_out_ch': torch.Tensor([first_conv_out_ch]),
             'decode_layers': torch.Tensor([decode_layers]),
-            'embeding_size': torch.Tensor(embeding_size),
+            'embeding_size': torch.Tensor(embedding_size),
             'drop_out': torch.Tensor([dropout])
         }
 
@@ -87,20 +96,22 @@ class CNNGRU(nn.Module):
             DoubleConv3d(2 ** i * first_conv_out_ch,
                          2 ** (i + 1) * first_conv_out_ch,
                          stride=[1, 2, 2], kern_size=[1, 3, 3], padding=[0, 1, 1], dropout=dropout)
-            for i in range(decode_layers - 1)
+            for i in range(decode_layers)
         ]
         self.decode = nn.Sequential(*_decode_layers)
-        self.adaptive_pool = nn.AdaptiveMaxPool3d([20] + self._embeding_size)
-        self.grus = BGRUStack(self._embeding_size[0] * self._embeding_size[1],
-                              out_ch, first_conv_out_ch * 2 ** decode_layers)
-        self.gru_out = BGRUCell(out_ch * 20 * 2, out_ch)
+        self.adaptive_pool = nn.AdaptiveMaxPool3d(list(embedding_size))
+        self.grus = BGRUStack(embedding_size[0] * embedding_size[1],
+                              out_ch, first_conv_out_ch * 2 ** decode_layers,
+                              num_layers=gru_layers)
+        self.gru_out = BGRUCell(out_ch * embedding_size[0] * 2, out_ch, num_layers=gru_layers)
 
-        self.fc_out = nn.Linear(512, 1)
+        self.fc_out = nn.Linear(first_conv_out_ch * 2 ** decode_layers, 1)
 
 
     def forward(self, x):
         while x.dim() < 5:
             x = x.unsqueeze(0)
+        x = x.contiguous()
         x = self.in_conv1(x)
         x = self.decode(x)
 
@@ -112,11 +123,11 @@ class CNNGRU(nn.Module):
         x = self.grus(x)
         x = x.permute(0, 2, 1, 3)
         x = x.contiguous()
-        x = x.view(*(list(x.shape[:-2]) + [-1]))
+        x = x.view(*(list(x.shape[:-2]) + [-1]))        # Merge last two axis
         x, _ = self.gru_out(x)
-        x = x.view(*(list(x.shape[:-1]) + [2, -1]))
-        x = x.mean(axis=[-2])
-        x = self.fc_out(x.permute(0, 2, 1)).squeeze()
+        x = x.view(*(list(x.shape[:-1]) + [2, -1]))     # Seperate +ve direction and -ve direction outputs
+        x = x.mean(axis=[-2])                           # Take average of them.
+        x = self.fc_out(x.permute(0, 2, 1)).squeeze()   # Linear combination of channel outputs.
 
         # Expand dim to (BxC)
         while x.dim() < 2:
@@ -125,3 +136,4 @@ class CNNGRU(nn.Module):
             else:
                 x = x.unsqueeze(0)
         return x
+
