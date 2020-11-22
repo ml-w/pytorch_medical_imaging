9227127e75aed7424073334353a98db9d5314cc5
Matthew Wong <fromosia@gmail.com>
2020-11-22 21:32:59 +0800

    Add function to match input with network.

--- pytorch_med_imaging/Inferencers/InferencerBase.py
+++ pytorch_med_imaging/Inferencers/InferencerBase.py
@@ -1,6 +1,7 @@
 import os
 from abc import abstractmethod
 from pytorch_med_imaging.logger import Logger
+import torch
 
 class InferencerBase(object):
     """
@@ -47,6 +48,51 @@ class InferencerBase(object):
         self._input_check()
         self._create_net()
         self._create_dataloader()
+
+
+    def _match_type_with_network(self, tensor):
+        """
+        Return a tensor with the same type as the first weight of `self._net`. This function seems to cause CUDA
+        error in pytorch 1.3.0
+
+        Args:
+            tensor (torch.Tensor or list): Input `torch.Tensor` or list of `torch.Tensor`
+
+        Returns:
+            out (torch.Tensor)
+        """
+        assert isinstance(tensor, list) or torch.is_tensor(tensor), "_match_type_with_network: input type error!"
+
+        for name, module in self._net.named_modules():
+            try:
+                self._net_weight_type = module.weight.type()
+                #self._logger.debug("Module type is: {}".format(self._net_weight_type))
+                break
+            except AttributeError:
+                continue
+            except Exception as e:
+                self._logger.log_print_tqdm("Unexpected error in type convertion of solver")
+
+        if self._net_weight_type is None:
+            # In-case type not found
+            self._logger.log_print_tqdm("Cannot identify network type, falling back to float type.")
+            self._net_weight_type = torch.FloatTensor
+
+        # Do nothing if type is already correct.
+        try:
+            if isinstance(tensor, list) or isinstance(tensor, tuple):
+                if all([t.type() == self._net_weight_type for t in tensor]):
+                    return tensor
+            else:
+                if tensor.type() == self._net_weight_type:
+                    return tensor
+        except:
+            self._logger.warning("Can't determine if type is already followed.")
+
+        # We also expect list input too.
+        out = [ss.type(self._net_weight_type) for ss in tensor] if isinstance(tensor, list) else \
+            tensor.type(self._net_weight_type)
+        return out
 
 
     def get_net(self):
