f6a3301c4ec8ef1df3412e6173dc0782a4b1b4e8
Matthew Wong <fromosia@gmail.com>
2021-01-25 20:03:09 +0800

    Add a tensor dataset that concat parallelly

--- pytorch_med_imaging/med_img_dataset/PMITensorDataset.py
+++ pytorch_med_imaging/med_img_dataset/PMITensorDataset.py
@@ -2,7 +2,7 @@ from . import PMIDataBase
 import torch
 from torch.utils.data import TensorDataset, Dataset
 
-__all__ = ['PMITensorDataset']
+__all__ = ['PMITensorDataset', 'PMIParallelConcatDataset']
 
 class PMITensorDataset(TensorDataset):
     def __init__(self, *tensors):
@@ -11,3 +11,13 @@ class PMITensorDataset(TensorDataset):
         """
         self.tensors = tensors
 
+class PMIParallelConcatDataset(Dataset):
+    def __init__(self, *tensors):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        self.tensors = tensors
+
+    def size(self, idx=None):
+        return self.tensors[0].size(idx)
+
+    def __getitem__(self, item):
+        return (t[item] for t in self.tensors)
--- pytorch_med_imaging/med_img_dataset/__init__.py
+++ pytorch_med_imaging/med_img_dataset/__init__.py
@@ -27,6 +27,6 @@ for row in f.readlines():
     except:
         add_private_dict_entry("CT-PD-dict_v8", Tag(*(r[0][1:-1]).split(',')), r[1].split('/')[0], r[2],r[2])
 
-__all__ = ['ImageFeaturePair', 'Landmarks', 'ImageDataSet',
+__all__ = ['ImageFeaturePair', 'Landmarks', 'ImageDataSet', 'PMIParallelConcatDataset',
            'Projection', 'Subbands', 'ImagePatchesLoader', 'ImageDataSetFilter',
            'ImageDataSetAugment', 'ImageDataMultiChannel', 'ImagePatchesLoader3D', 'DataLabel']
