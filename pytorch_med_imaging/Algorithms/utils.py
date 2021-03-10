835a2894c4764774b36e54dc948582eda23d2912
Matthew Wong <fromosia@gmail.com>
2021-03-10 21:18:02 +0800

    Fix bugs and add logger to utils.py in Algorithm

--- pytorch_med_imaging/Algorithms/utils.py
+++ pytorch_med_imaging/Algorithms/utils.py
@@ -1,6 +1,9 @@
 import re, os
+from pytorch_med_imaging.logger import Logger
 
 __all__ = ['get_unique_IDs', 'get_fnames_by_globber', 'get_fnames_by_IDs', 'load_supervised_pair_by_IDs']
+
+
 
 def get_unique_IDs(fnames, globber=None):
     idlist = []
@@ -18,6 +21,7 @@ def get_unique_IDs(fnames, globber=None):
 
 
 def get_fnames_by_IDs(fnames, idlist, globber=None):
+    _logger = Logger['algorithm.utils']
     if globber is None:
         globber = "([0-9]{3,5})"
 
@@ -25,11 +29,16 @@ def get_fnames_by_IDs(fnames, idlist, globber=None):
     for id in idlist:
         flist = []
         for f in fnames:
-            l = re.findall(globber, f)
+            _f = os.path.basename(f)
+            l = re.findall(globber, _f)
             if not len(l):
                 continue
             if l[0] == id:
                 flist.append(f)
+        # skip if none is found
+        if len(flist) == 0:
+            _logger.warning(f"Can't found anything for key {id}. Skipping..")
+            continue
         outfnames[id] = flist
     return outfnames
 
@@ -43,15 +52,29 @@ def get_fnames_by_globber(fnames, globber):
             copy.remove(f)
     return copy
 
+
 def load_supervised_pair_by_IDs(source_dir, target_dir, idlist, globber=None):
     source_list = get_fnames_by_globber(os.listdir(source_dir), globber) \
         if not globber is None else os.listdir(source_dir)
+    _logger = Logger['algorithm.utils']
+
     source_list = get_fnames_by_IDs(source_list, idlist)
+    source_keys = source_list.keys()
     source_list = [source_list[key][0] for key in source_list]
     target_list = get_fnames_by_IDs(os.listdir(target_dir), idlist)
-    target_list = [target_list[key][0] for key in target_list]
+    target_keys = target_list.keys()
+    target_list = [target_list[key][0] for key in source_keys]
 
     if len(source_list) != len(target_list):
+        _logger.error("Dimension mismatch when pairing.")
+        missing = {'Src': [], 'Target': []}
+        for src in source_keys:
+            if src not in target_keys:
+                missing['Src'].append(src)
+        for tar in target_keys:
+            if tar not in source_keys:
+                missing['Target'].append(src)
+        _logger.debug(f"{missing}")
         raise ValueError("Dimension mismatch! Src: %i vs Target: %i"%(len(source_list), len(target_list)))
 
     return source_list, target_list
@@ -161,6 +184,7 @@ def directory_index(dir, out_csv, id_globber="(^[a-zA-Z0-9]+)"):
         outdf = outdf.append(row)
 
     outdf.to_csv(out_csv, index=False)
+
 
 if __name__ == '__main__':
     # directory_sorter('../NPC_Segmentation/0A.NIFTI_ALL/Malignant')
