f904ddeed6a2ff751d65347ec5518375dcfde3c0
Matthew Wong <fromosia@gmail.com>
2021-05-11 17:51:48 +0800

    Update label remapping function

--- pytorch_med_imaging/utils/preprocessing_labelmaps.py
+++ pytorch_med_imaging/utils/preprocessing_labelmaps.py
@@ -7,7 +7,8 @@ import tqdm.auto as auto
 
 def remap_label(map_dict: dict,
                 inputdata: ImageDataSet,
-                output_dir: str):
+                output_dir: str,
+                num_workers: int = 8):
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
@@ -16,11 +17,8 @@ def remap_label(map_dict: dict,
     data = tio.SubjectsDataset([tio.Subject(seg=d) for d in inputdata.data],
                                transform=tio.RemapLabels(remapping=map_dict))
     # using worker might cause overloading shared mem pool at /dev/shm, set to 0
-    dataloader = DataLoader(data, batch_size=1, num_workers=0)
+    dataloader = DataLoader(data, batch_size=1, num_workers=num_workers)
 
-    out_tensor =[]
     for i, dat in enumerate(auto.tqdm(dataloader)):
-        out_tensor.append(dat['seg'][tio.DATA].permute(0, 1, 4, 3, 2)) # (B x C x W x H x D) to (B x C x D x W x H)
-
-    inputdata.write_all(out_tensor, output_dir)
-
+        o = dat['seg'][tio.DATA].permute(0, 1, 4, 3, 2) # (B x C x W x H x D) to (B x C x D x W x H)
+        inputdata.write_uid(o, i, output_dir)
