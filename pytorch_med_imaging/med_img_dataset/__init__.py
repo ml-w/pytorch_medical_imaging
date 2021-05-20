20c188c840f72bb657ea12fa16d3e6043694f5f3
Matthew Wong <fromosia@gmail.com>
2021-05-20 15:29:19 +0800

    Remove some classes

--- pytorch_med_imaging/med_img_dataset/__init__.py
+++ pytorch_med_imaging/med_img_dataset/__init__.py
@@ -1,16 +1,7 @@
 from .ImageData import ImageDataSet
-from .ImageDataFilter import ImageDataSetFilter
-from .ImageDataAugment import ImageDataSetAugment
-from .ImagePatchesLoader import ImagePatchesLoader
-from .ImagePatchesLoader3D import ImagePatchesLoader3D
-from .ImageFeaturePair import ImageFeaturePair
 from .ImageDataMultiChannel import ImageDataMultiChannel
 from .DataLabel import DataLabel
-from .Subbands import Subbands
 from .Landmarks import Landmarks
-from .Projection import Projection
-from .Projection_v2 import Projection_v2
-from .DataLabel import DataLabel
 from pydicom.datadict import add_dict_entry, add_private_dict_entry
 from pydicom.tag import Tag
 from os.path import abspath, basename
@@ -28,7 +19,5 @@ for row in f.readlines():
     except:
         add_private_dict_entry("CT-PD-dict_v8", Tag(*(r[0][1:-1]).split(',')), r[1].split('/')[0], r[2],r[2])
 
-__all__ = ['ImageFeaturePair', 'Landmarks', 'ImageDataSet', 'PMIParallelConcatDataset',
-           'Projection', 'Subbands', 'ImagePatchesLoader', 'ImageDataSetFilter',
-           'ImageDataSetAugment', 'ImageDataMultiChannel', 'ImagePatchesLoader3D', 'DataLabel',
+__all__ = ['ImageDataSet', 'ImageDataMultiChannel', 'DataLabel',
            'PMIDataBase']
--- pytorch_med_imaging/med_img_dataset/computations/__init__.py
+++ pytorch_med_imaging/med_img_dataset/computations/__init__.py
@@ -1,11 +1,5 @@
 from .LocalBinaryPattern import lbp
 from .LocalNeighborhoodDifferencePattern import lndp
-from .ImagePatchLocMMTex import *
-from .ImageDataSetWithPos import *
-from .ImageDataWithTexture import *
-from .ImagePatchLocTex import *
-from .ImagePatchLocMMTex import *
 from .ProbFunc import clip_5
 
-__all__ = ['ImageDataSetWithPos', 'ImagePatchLocTex', 'lbp', 'lndp', 'ImagePatchLocMMTex',
-           'ImageDataSetWithPos', 'clip_5', 'ImageDataSetWithTexture']
+__all__ = [ 'lbp', 'lndp', 'clip_5', 'ImageDataSetWithTexture']
