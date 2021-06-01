aff9c389d8017670bdf11ab56bea9ec4c1c62de6
Matthew Wong <fromosia@gmail.com>
2021-06-01 14:15:55 +0800

    Add option to normalize the pixel counts, note that background is ignored.

--- pytorch_med_imaging/scripts/preprocessing_labelmaps.py
+++ pytorch_med_imaging/scripts/preprocessing_labelmaps.py
@@ -26,7 +26,11 @@ def remap_label():
 
 def pmi_label_statistics():
     parser = pmi_console_entry('iOgLnv')
+    parser.add_argument('--normalize', action='store_true', help="Normalize the pixel count.")
     args = parser.parse_args()
 
-    df = label_statistics(args.input, args.idglobber, args.numworker, verbose=args.verbose)
-    df.to_csv(args.outfile)
+    df = label_statistics(args.input, args.idglobber, args.numworker, verbose=args.verbose, normalized=args.normalize)
+    if args.outfile.endswith('.csv'):
+        df.to_csv(args.outfile)
+    else:
+        df.to_excel(args.outfile)
