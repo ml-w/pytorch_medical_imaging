c56b615f4d61fdcf259884c63edb17ff82adcb29
alabamagan <fromosia@gmail.com>
2022-05-16 19:59:53 +0800

    A more detailed test for ConfidenceBCELoss

--- Test/UnitTests/Test_losses.py
+++ Test/UnitTests/Test_losses.py
@@ -9,21 +9,43 @@ class TestLoss(unittest.TestCase):
         import torch
         import torch.autograd as autograd
 
-        loss_func = ConfidenceBCELoss()
-        test_target = torch.DoubleTensor([1, 1, 0, 0, 1]).view(5, -1)
-        test_input = torch.DoubleTensor([[10.4, 23.6, 1],
-                                         [3.8, 10.2, 1],
-                                         [-1.9, 0.9, 1],
-                                         [10, -5, 1],
-                                         [123, 100, 1]]).view(5, -1)
+        loss_func = ConfidenceBCELoss(conf_factor=1)
+        test_target = torch.FloatTensor([1, 1, 0, 0, 1]).view(5, -1)
+        test_input_1 = torch.FloatTensor([[0.99, 0.9, 1],
+                                          [0.90, 0.9, 1],
+                                          [0.01, 0.9, 1],
+                                          [0.10, 0.9, 1],
+                                          [0.99, 0.9, 1]]).view(5, -1) # High confidence correct
+        test_input_2 = torch.FloatTensor([[0.1, 0.9, 1],
+                                          [0.1, 0.9, 1],
+                                          [0.9, 0.9, 1],
+                                          [0.9, 0.9, 1],
+                                          [0.1, 0.9, 1]]).view(5, -1) # High confidence wrong
+        test_input_3 = torch.FloatTensor([[0.1, 0.1, 1],
+                                          [0.1, 0.1, 1],
+                                          [0.9, 0.1, 1],
+                                          [0.9, 0.1, 1],
+                                          [0.1, 0.1, 1]]).view(5, -1) # Low confidence wrong
+        test_input_4 = torch.FloatTensor([[0.9, 0.1, 1],
+                                          [0.9, 0.1, 1],
+                                          [0.1, 0.1, 1],
+                                          [0.1, 0.1, 1],
+                                          [0.9, 0.1, 1]]).view(5, -1) # Low confidence right
+
+        test_inputs = [test_input_1,
+                       test_input_2,
+                       test_input_3,
+                       test_input_4]
         test_target = autograd.Variable(test_target, requires_grad=True)
-        test_input = autograd.Variable(test_input, requires_grad=True)
+        for i, t in enumerate(test_inputs):
+            test_inputs[i] = autograd.Variable(t, requires_grad=True)
 
         if torch.cuda.is_available():
             loss_func = loss_func.cuda()
             test_target = test_target.cuda()
-            test_input = test_input.cuda()
+            for i, t in enumerate(test_inputs):
+                test_inputs[i] = t.cuda()
 
-        loss = loss_func(test_input, test_target)
-        loss.backward()
+        losses = [loss_func(t, test_target) for t in test_inputs]
+        print(torch.stack(losses))
 
