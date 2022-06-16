e0706c1d6fb77361924c53fe2fc8dd24520e24a0
alabamagan <fromosia@gmail.com>
2022-06-16 13:42:09 +0800

    Imporve loss function test to also validate the dimension of the loss outputs

--- Test/UnitTests/Test_losses.py
+++ Test/UnitTests/Test_losses.py
@@ -5,57 +5,62 @@ class TestLoss(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestLoss, self).__init__(*args, **kwargs)
 
-    def test_ConfBCELoss(self):
+    def setUp(self) -> None:
         import torch
         import torch.autograd as autograd
 
+        self.test_target = torch.FloatTensor([1, 1, 0, 0, 1]).view(5, -1)
+        self.test_input_1 = torch.FloatTensor([[0.99, 0.9, 1],
+                                               [0.90, 0.9, 1],
+                                               [0.01, 0.9, 1],
+                                               [0.10, 0.9, 1],
+                                               [0.99, 0.9, 1]]).view(5, -1) # High confidence correct
+        self.test_input_2 = torch.FloatTensor([[0.1, 0.9, 1],
+                                               [0.1, 0.9, 1],
+                                               [0.9, 0.9, 1],
+                                               [0.9, 0.9, 1],
+                                               [0.1, 0.9, 1]]).view(5, -1) # High confidence wrong
+        self.test_input_3 = torch.FloatTensor([[0.1, 0.1, 1],
+                                               [0.1, 0.1, 1],
+                                               [0.9, 0.1, 1],
+                                               [0.9, 0.1, 1],
+                                               [0.1, 0.1, 1]]).view(5, -1) # Low confidence wrong
+        self.test_input_4 = torch.FloatTensor([[0.9, 0.1, 1],
+                                               [0.9, 0.1, 1],
+                                               [0.1, 0.1, 1],
+                                               [0.1, 0.1, 1],
+                                               [0.9, 0.1, 1]]).view(5, -1) # Low confidence right
+        self.test_input_5 = torch.FloatTensor([0.9,
+                                               0.1,
+                                               0.1,
+                                               0.9,
+                                               0.9]).view(5, -1)
+
+        self.test_inputs = [self.test_input_1,
+                            self.test_input_2,
+                            self.test_input_3,
+                            self.test_input_4,
+                            self.test_input_5]
+        self.test_target = autograd.Variable(self.test_target, requires_grad=True)
+        for i, t in enumerate(self.test_inputs):
+            self.test_inputs[i] = autograd.Variable(t, requires_grad=True)
+
+    def test_ConfBCELoss(self):
         loss_func = ConfidenceBCELoss(conf_factor=1)
-        test_target = torch.FloatTensor([1, 1, 0, 0, 1]).view(5, -1)
-        test_input_1 = torch.FloatTensor([[0.99, 0.9, 1],
-                                          [0.90, 0.9, 1],
-                                          [0.01, 0.9, 1],
-                                          [0.10, 0.9, 1],
-                                          [0.99, 0.9, 1]]).view(5, -1) # High confidence correct
-        test_input_2 = torch.FloatTensor([[0.1, 0.9, 1],
-                                          [0.1, 0.9, 1],
-                                          [0.9, 0.9, 1],
-                                          [0.9, 0.9, 1],
-                                          [0.1, 0.9, 1]]).view(5, -1) # High confidence wrong
-        test_input_3 = torch.FloatTensor([[0.1, 0.1, 1],
-                                          [0.1, 0.1, 1],
-                                          [0.9, 0.1, 1],
-                                          [0.9, 0.1, 1],
-                                          [0.1, 0.1, 1]]).view(5, -1) # Low confidence wrong
-        test_input_4 = torch.FloatTensor([[0.9, 0.1, 1],
-                                          [0.9, 0.1, 1],
-                                          [0.1, 0.1, 1],
-                                          [0.1, 0.1, 1],
-                                          [0.9, 0.1, 1]]).view(5, -1) # Low confidence right
-        test_input_5 = torch.FloatTensor([0.9,
-                                          0.1,
-                                          0.1,
-                                          0.9,
-                                          0.9]).view(5, -1)
-
-        test_inputs = [test_input_1,
-                       test_input_2,
-                       test_input_3,
-                       test_input_4,
-                       test_input_5]
-        test_target = autograd.Variable(test_target, requires_grad=True)
-        for i, t in enumerate(test_inputs):
-            test_inputs[i] = autograd.Variable(t, requires_grad=True)
-
         if torch.cuda.is_available():
             loss_func = loss_func.cuda()
-            test_target = test_target.cuda()
-            for i, t in enumerate(test_inputs):
-                test_inputs[i] = t.cuda()
+            self.test_target = self.test_target.cuda()
+            for i, t in enumerate(self.test_inputs):
+                self.test_inputs[i] = t.cuda()
 
-        for t in test_inputs:
+        for t in self.test_inputs:
+            msg = f"Loss function failed when handling input with shape ({t.shape}): \n{t}"
+            loss = loss_func(t, self.test_target)
+            self.assertEqual(1, loss.dim(), msg=msg)
+            self.assertEqual(1, loss.shape[0], msg=msg)
             try:
-                loss_func(t, test_target)
-            except:
-                msg = f"Loss function failed when handling input with shape ({t.shape}): {t} "
+                loss.backward()
+            except Exception as e:
+                msg += f"The loss shape was: {loss.shape}"
                 self.fail(msg)
 
