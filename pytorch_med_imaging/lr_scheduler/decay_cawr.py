d5d2421a8235dfcec6f849b37c1337896f81566f
alabamagan <fromosia@gmail.com>
2022-06-23 11:36:51 +0800

    Fix DecayCAWR

--- pytorch_med_imaging/lr_scheduler/decay_cawr.py
+++ pytorch_med_imaging/lr_scheduler/decay_cawr.py
@@ -2,17 +2,14 @@ from torch.optim import lr_scheduler
 
 __all__ = ['DecayCAWR']
 
-class DecayCAWR(lr_scheduler.ChainedScheduler):
+class DecayCAWR(lr_scheduler.CosineAnnealingWarmRestarts):
     def __init__(self, optimizer, exp_factor, T_0, T_mult=1, eta_min=0, last_epoch=-1, verbose=False):
-        assert 0 < exp_factor < 1.0, f"Exponential factor must be between 0 and 1, got {exp_factor} instead"
-        lambdaLR = lr_scheduler.LambdaLR(optimizer,
-                                         lambda epoch: exp_factor ** epoch,
-                                         last_epoch=last_epoch,
-                                         verbose=verbose)
-        CAWR = lr_scheduler.CosineAnnealingWarmRestarts(optimizer,
-                                                        T_0,
-                                                        T_mult,
-                                                        eta_min,
-                                                        last_epoch,
-                                                        verbose)
-        super(DecayCAWR, self).__init__([lambdaLR, CAWR])
+        if not (0 < exp_factor < 1.0):
+            raise ValueError("Expected exp_factor to be between 0 - 1, but got {}.".format(exp_factor))
+        self.exp_factor = exp_factor
+        super(DecayCAWR, self).__init__(optimizer, T_0, T_mult=T_mult, eta_min=eta_min, last_epoch=last_epoch,
+                                        verbose=verbose)
+
+    def get_lr(self):
+        base_lr = super(DecayCAWR, self).get_lr()
+        return [blr * self.exp_factor ** self.last_epoch for blr in base_lr]
