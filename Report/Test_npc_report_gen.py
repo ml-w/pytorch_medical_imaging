f70671ddb738b674572b49faf61e874610e85d33
alabamagan <fromosia@gmail.com>
2022-07-19 19:48:36 +0800

    Update unittest test data structure

--- Report/Test_npc_report_gen.py
+++ Report/Test_npc_report_gen.py
@@ -10,6 +10,7 @@ from npc_report_gen.rgio import get_t2w_series_files, process_input
 from pathlib import Path
 from mnts.mnts_logger import MNTSLogger
 
+
 class Test_pipeline(unittest.TestCase):
     def setUp(self):
         self._logger = MNTSLogger('.', 'test_report', verbose=True, keep_file=False, log_level='debug')
@@ -24,7 +25,7 @@ class Test_pipeline(unittest.TestCase):
         MNTSLogger.cleanup()
 
     def test_get_t2w_series(self):
-        p = Path("example_data/npc_case/ALL_DICOM")
+        p = Path("test_data/npc_case/ALL_DICOM")
         files = get_t2w_series_files(str(p.absolute()))
         self.assertEqual(len(files) > 0, True)
 
@@ -53,12 +54,12 @@ class Test_pipeline(unittest.TestCase):
         temp_out_dir2.cleanup()
 
     def test_main(self):
-        # p = Path("example_data/npc_case/ALL_DICOM")
+        # p = Path("test_data/npc_case/ALL_DICOM")
         # p = Path("/media/storage/Source/Repos/NPC_Segmentation/NPC_Segmentation/00.RAW/extra_20210426/T1rhoNPC020")
         p = Path("/mnt/ftp_shared/NPC_New_case/2005-2021_not into T1rho studies/1719")
         # pp = Path("/mnt/ftp_shared/NPC_Screening_3_plain/Images/P385")
         # p = Path("/home/lwong/FTP/2.Projects/8.NPC_Segmentation/00.RAW/NPC_new_dx_cases/1249/")
-        # p = Path("example_data/npc_case/1183-T2_FS_TRA+301.nii.gz")
+        # p = Path("test_data/npc_case/1183-T2_FS_TRA+301.nii.gz")
         # p = Path("/media/storage/Source/Repos/NPC_Segmentation/NPC_Segmentation/00.RAW/HKU/0140")
         # p = Path('/media/storage/Data/NPC_Segmentation/70.Screening_report/TestInput/')
         # p = Path('/home/lwong/Desktop/NPC_Segmentation/NPC_Segmentation/0A.NIFTI_ALL/HKU/temp/T2WFS_TRA')
@@ -83,10 +84,9 @@ class Test_pipeline(unittest.TestCase):
                 self._logger.exception(e)
                 self.fail(f"Something went wrong for {str(pp)}")
 
-
     def test_post_proc_main(self):
         import time
-        p = Path("example_data/npc_case/1183.nii.gz")
+        p = Path("test_data/npc_case/NIFTI/seg/eg01.nii.gz")
 
         with tempfile.TemporaryDirectory() as temp_dir:
             _in = Path(temp_dir).joinpath('input')
@@ -95,20 +95,19 @@ class Test_pipeline(unittest.TestCase):
             _out.mkdir()
             shutil.copy2(str(p), str(_in))
 
-            # command = f"-i {str(_in)} -o {str(_out)} -v".split()
             seg_post_main(_in, _out)
 
             self.assertTrue(len(os.listdir(str(_out))) > 0)
 
     def test_segmentation(self):
-        p = Path('example_data/npc_case/')
+        p = Path('test_data/npc_case/')
         with tempfile.TemporaryDirectory() as temp_dir:
             mask_dir = Path(temp_dir).joinpath('mask')
             img_dir = Path(temp_dir).joinpath('img')
             mask_dir.mkdir(exist_ok=True)
             img_dir.mkdir(exist_ok=True)
-            shutil.copy2(str(p.joinpath('1183.nii.gz')), str(mask_dir))
-            shutil.copy2(str(p.joinpath('1183-T2_FS_TRA+301.nii.gz')), str(img_dir))
+            shutil.copy2(str(p.joinpath('NIFTI/seg/eg01.nii.gz')), str(mask_dir))
+            shutil.copy2(str(p.joinpath('NIFTI/img/eg01.nii.gz')), str(img_dir))
 
             override_tags = {
                 '(Data,input_dir)': str(img_dir),
@@ -121,14 +120,14 @@ class Test_pipeline(unittest.TestCase):
             pmi_main(command)
 
     def test_dl_diag(self):
-        p = Path('example_data/npc_case/')
+        p = Path('test_data/npc_case/')
         with tempfile.TemporaryDirectory() as temp_dir:
             mask_dir = Path(temp_dir).joinpath('mask')
             img_dir = Path(temp_dir).joinpath('img')
             mask_dir.mkdir(exist_ok=True)
             img_dir.mkdir(exist_ok=True)
-            shutil.copy2(str(p.joinpath('1183.nii.gz')), str(mask_dir))
-            shutil.copy2(str(p.joinpath('1183-T2_FS_TRA+301.nii.gz')), str(img_dir))
+            shutil.copy2(str(p.joinpath('NIFTI/seg/eg01.nii.gz')), str(mask_dir))
+            shutil.copy2(str(p.joinpath('NIFTI/img/eg01.nii.gz')), str(img_dir))
 
             override_tags = {
                 '(Data,input_dir)': str(img_dir),
@@ -142,7 +141,7 @@ class Test_pipeline(unittest.TestCase):
             print(list(Path(temp_dir).joinpath("output").joinpath('class_inf.csv').open('r').readlines()))
 
     def test_process_output(self):
-        p = Path('./example_data/report_gen')
+        p = Path('./test_data/report_gen')
         generate_report(p, self.temp_output_path.name)
 
 
