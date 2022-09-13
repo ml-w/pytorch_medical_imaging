0600e7855edca916eb0e7b1f8a7a4cc3be2d6036
alabamagan <fromosia@gmail.com>
2022-09-13 23:33:04 +0800

    Update __init__.py for PMI_data_loader

--- pytorch_med_imaging/PMI_data_loader/__init__.py
+++ pytorch_med_imaging/PMI_data_loader/__init__.py
@@ -2,6 +2,7 @@ from .pmi_image_dataloader import *
 from .pmi_batch_sampler_factory import *
 from .pmi_batch_zero_pad_sampler import *
 from .pmi_data_factory import *
+from .pmi_img_feat_pair_dataloader import *
 
 from .lambda_tio_adaptor import *
 from .augmenter_factory import *
