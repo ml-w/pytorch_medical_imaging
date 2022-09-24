f7b132c6eef5367293828d400d711f97717b501b
alabamagan <fromosia@gmail.com>
2022-09-24 13:32:49 +0800

    Fix error when no validation percentage is specified.

--- pytorch_med_imaging/scripts/gen_batch.py
+++ pytorch_med_imaging/scripts/gen_batch.py
@@ -11,7 +11,7 @@ def console_entry(raw_args=None):
                         help='Output directory to folds')
     parser.add_argument('-g', '--idglobber', action='store', default=None, dest='idglobber',
                         help='If a directory holding nii files is provided ')
-    parser.add_argument('-t', '--validation', action='store', default=None, type=float,
+    parser.add_argument('-t', '--validation', action='store', default=0, type=float,
                         help="Set validation percentage.")
     parser.add_argument('-m', '--num-of-folds', action='store', type=int, required=True,
                         help="Number of folds to create. If 1, its a train-test split.")
@@ -27,14 +27,14 @@ def console_entry(raw_args=None):
     with MNTSLogger('./default.log', logger_name='dicom2nii', verbose=True, keep_file=a.log) as logger:
         logger.info("Recieve argumetns: {}".format(a))
         if a.validation is not None:
-            assert 0 < a.validation < 1, "Option --validaiton must be between 0 and 1."
+            assert 0 <= a.validation <= 1, "Option --validaiton must be between 0 and 1."
 
         # if input is excel or csv
         input_dir = Path(a.input)
         output_dir = Path(a.output)
         if input_dir.suffix in ('.csv', '.xlsx'):
             # take first column as ids
-            if input_dir.suffix == 'csv':
+            if input_dir.suffix == '.csv':
                 table = pd.read_csv(str(input_dir.resolve()), index_col=0)
             else:
                 table = pd.read_excel(str(input_dir.resolve()), index_col=0)
@@ -45,7 +45,7 @@ def console_entry(raw_args=None):
         GenerateTestBatch(table.index,
                           a.num_of_folds,
                           output_dir.__str__(),
-                          stratification_class=table[a.stratification],
+                          stratification_class=table[a.stratification] if not a.stratification is None else None,
                           validation=int(len(table) * a.validation) if not a.validation is None else None,
                           prefix=a.prefix
                           )
