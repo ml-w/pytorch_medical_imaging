842b25106b012581d57f959e2394866a236b4e01
alabamagan <fromosia@gmail.com>
2022-10-17 11:49:20 +0800

    Fix some of the unittests

--- Test/UnitTests/Test_network.py
+++ Test/UnitTests/Test_network.py
@@ -12,7 +12,8 @@ class Test3DNetworks(unittest.TestCase):
 
     def setUp(self) -> None:
         num_slice = 30
-        num_data = 2
+        num_data = 4
+        self.sample_input_big = torch.rand(num_data, 1, 512, 512, num_slice).cuda()
         self.sample_input = torch.rand(num_data, 1, 128, 128, num_slice).cuda()
         self.sample_input_size1 = torch.rand(1, 1, 128, 128, num_slice).cuda()
         self.sample_input_3d = torch.rand(num_data, 1, 128, 128, 128).cuda()
@@ -28,7 +29,36 @@ class Test3DNetworks(unittest.TestCase):
         self.expect_nonzero[1, ..., 7:16] = True
 
     def test_rAIdiologist(self):
-        net = rAIdiologist(record=False).cuda()
+        net = rAIdiologist(1, record=False).cuda()
+        with torch.no_grad():
+            for i in range(6):
+                try:
+                    net.set_mode(i)
+                    self.assertTrue(net._mode == i)
+                    out = net(self.sample_input)
+                    self.assertEqual(2, out.dim(), "Failed during forward.")
+                    out = net(self.sample_input_size1)
+                    self.assertEqual(2, out.dim(), "Failed for batch-size = 1.")
+                    print(f"Mode {i} passed.")
+                except Exception as e:
+                    self.fail(f"Mode {i} error. Original message {e}")
+
+
+    def test_rAIdiologist_ch1(self):
+        net = rAIdiologist(out_ch=1, record=False).cuda()
+        with torch.no_grad():
+            for i in range(6):
+                try:
+                    net.set_mode(i)
+                    self.assertTrue(net._mode == i)
+                    out = net(self.sample_input)
+                    self.assertEqual(2, out.dim())
+                    print(f"Mode {i} passed. Out size: {out.shape}")
+                except Exception as e:
+                    self.fail(f"Mode {i} error. Original message {e}")
+
+    def test_rAIdiologist_v2(self):
+        net = rAIdiologist_v2(record=False).cuda()
         with torch.no_grad():
             for i in range(6):
                 try:
@@ -39,70 +69,6 @@ class Test3DNetworks(unittest.TestCase):
                     print(f"Mode {i} passed.")
                 except:
                     self.fail(f"Mode {i} error.")
-
-    def test_rAIdiologist_focal(self):
-        _inter_mediate_data = []
-        def _get_input_hook(module, input, _):
-            r"""This hook cleans the rAIdiologist playback list prior to running a mini-batch"""
-            _inter_mediate_data.append(input[0].permute(0, 2, 1))
-
-        net = rAIdiologist(record=False).cuda()
-        handler = net.lstm_prelayernorm.register_forward_hook(_get_input_hook)
-        with torch.no_grad():
-            for mode in (1, 2):
-                net.set_mode(mode)
-                out = net(self.sample_input, self.sample_seg)
-                temp_out = _inter_mediate_data[0]
-                bool_index = self.expect_nonzero
-                print(out.shape)
-                # Assert the temp_out is zero where the segmentation is zero
-                self.assertEqual(0, temp_out[~bool_index.expand_as(temp_out)].sum())
-                # Assert the temp out is not zero where the segmetnation is not zero
-                self.assertNotEqual(0, temp_out[bool_index.expand_as(temp_out)].sum())
-                _inter_mediate_data.clear()
-
-                # raise error if there are no segmentation (all zeros)
-                zeros = torch.zeros_like(self.sample_input)
-                zeros[0] = 1 # One of the batch member wasn't all zero
-                with self.assertRaises(ArithmeticError):
-                    net(self.sample_input, zeros)
-                _inter_mediate_data.clear()
-                print(f"Mode {mode} passed")
-            for mode in (3, 4, 5):
-                net.set_mode(mode)
-                out = net(self.sample_input, self.sample_seg)
-                for temp_out in _inter_mediate_data:
-                    # None of the inputs should contain slices with all zeros
-                    self.assertFalse(0 in list(temp_out.sum(dim=[0, 1])))
-                _inter_mediate_data.clear()
-
-                zeros = torch.zeros_like(self.sample_input)
-                zeros[0] = 1 # One of the batch member wasn't all zero
-                with self.assertRaises(ArithmeticError):
-                    net(self.sample_input, zeros)
-                _inter_mediate_data.clear()
-
-                # check if things are correct when there's only one slice with nonzero
-                zeros = torch.zeros_like(self.sample_input)
-                zeros[0, ..., 12] = 1
-                zeros[1, ..., 13] = 1
-                out = net(self.sample_input, zeros)
-                print(f"{out.shape}")
-                print(f"Mode {mode} passed")
-
-    def test_rAIdiologist_focal_size1(self):
-        _inter_mediate_data = []
-        def _get_input_hook(module, input, _):
-            r"""This hook cleans the rAIdiologist playback list prior to running a mini-batch"""
-            _inter_mediate_data.append(input[0].permute(0, 2, 1))
-
-        net = rAIdiologist(record=False).cuda()
-        handler = net.lstm_prelayernorm.register_forward_hook(_get_input_hook)
-        with torch.no_grad():
-            for mode in (1, 2, 3, 4, 5):
-                net.set_mode(mode)
-                out = net(self.sample_input_size1, self.sample_seg_size1)
-                self.assertEqual(2, out.dim())
 
     def test_rAIdiologist_record(self):
         net = rAIdiologist(record=True).cuda()
@@ -140,3 +106,34 @@ class Test3DNetworks(unittest.TestCase):
             self.assertEqual(self.sample_input_3d_size1.shape[2:], out.shape[2:])
             with self.assertRaises(AssertionError):
                 net(self.sample_input)
+
+    def test_ViT_VNet(self):
+        from pytorch_med_imaging.networks.third_party_nets import ViTVNet
+        net = ViTVNet(img_size = self.sample_input_big.shape[2:]).cuda()
+        with torch.no_grad():
+            out = net(self.sample_input_big)
+
+    def test_SlicewiseRAN(self):
+        from pytorch_med_imaging.networks.specialized.slicewise_ran import SlicewiseAttentionRAN
+        for mode in SlicewiseAttentionRAN._strats_dict:
+            net = SlicewiseAttentionRAN(1, 5, reduce_strats=mode).cuda()
+            with torch.no_grad():
+                out = net(self.sample_input)
+                print(f"Mode {mode} passed.")
+
+    def test_RAN25D(self):
+        from pytorch_med_imaging.networks.specialized.slicewise_ran import AttentionRAN_25D, AttentionRAN_25D_MSE
+        net = AttentionRAN_25D(1, 4).cuda()
+        with torch.no_grad():
+            out = net(self.sample_input)
+            print(out.shape)
+
+        net = AttentionRAN_25D(1, 1).cuda()
+        with torch.no_grad():
+            out = net(self.sample_input)
+            print(out.shape)
+
+        net = AttentionRAN_25D_MSE(1, 1, (1, 4)).cuda()
+        with torch.no_grad():
+            out = net(self.sample_input)
+            print(out.shape)
