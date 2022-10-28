e71907fa91e6c15cdd97896dc8983c7581ef772e
alabamagan <fromosia@gmail.com>
2022-10-28 18:24:45 +0800

    Add support for bidirectional LSTM with zero padding using packed_padded_sequence function

--- pytorch_med_imaging/networks/specialized/rAIdiologist.py
+++ pytorch_med_imaging/networks/specialized/rAIdiologist.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, unpack_sequence, pad_packed_sequence
 import torchio as tio
 import os
 from pathlib import Path
@@ -29,8 +30,8 @@ class rAIdiologist(nn.Module):
         # LSTM for
         # self.lstm_prefc = nn.Linear(2048, 512)
         self.lstm_prelayernorm = nn.LayerNorm(2048)
-        self.lstm_rater = Transformer_rater(2048, out_ch=out_ch, embed_ch=128, record=record,
-                                            iter_limit=iter_limit, dropout=lstm_dropout)
+        self.lstm_rater = LSTM_rater(2048, out_ch=out_ch, embed_ch=128, record=record,
+                                     iter_limit=iter_limit, dropout=lstm_dropout)
 
         # Mode
         self.register_buffer('_mode', torch.IntTensor([1]))
@@ -137,17 +138,15 @@ class rAIdiologist(nn.Module):
 
         x = self.cnn(x)     # Shape -> (B x 2048 x S)
         x = self.dropout(x)
-
         while x.dim() < 3:
             x = x.unsqueeze(0)
+        x = x.permute(0, 2, 1).contiguous() # (B x C x S) -> (B x S x C)
+
+        # o: (B x S x out_ch)
+        o = self.lstm_rater(self.lstm_prelayernorm(x).permute(0, 2, 1),
+                            torch.as_tensor(top_slices).int() + 1).contiguous()
 
         if self._mode == 1 or self._mode == 2:
-            # o: (B x S x out_ch)
-            o = self.lstm_rater(self.lstm_prelayernorm(x.permute(0, 2, 1).contiguous()).permute(0, 2, 1),
-                                ~padding_mask).contiguous()
-            # max pool along slice dimension by permuting it to the last axis first, in this mode the confidence
-            # is ignored.
-
             # (B x S x C) -> (B x C x S) -> (B x C)
             # !!this following line is suppose to be correct, but there's a bug in torch such that it returns a vector
             # !!with same value when C = 1
@@ -159,11 +158,9 @@ class rAIdiologist(nn.Module):
             # o = torch.stack([o[i,j] for i, j in zero_slices.items()], dim=0)
         elif self._mode == 3 or self._mode == 4 or self._mode == 5:
             # Loop batch
-            o = []
+            _o = []
 
-            _o = self.lstm_rater(self.lstm_prelayernorm(x.permute(0, 2, 1).contiguous()).permute(0, 2, 1),
-                                 ~padding_mask).contiguous() # (B x S x 2 x C)
-            num_ch = _o.shape[-1]
+            num_ch = o.shape[-1]
 
             for i in nonzero_slices:
                 # output of bidirectional LSTM: (B x S x 2 x C), where for 3rd dim, 0 is forward and 1 is reverse
@@ -175,19 +172,19 @@ class rAIdiologist(nn.Module):
                 #    [0, 0, 4, 3, 2, 1, 0]]
                 # Therefore, for forward run, gets the element before padding, and for the reverse run, get the
                 # last element.
-                _o_forward  = torch.narrow(_o[i, : , 0], 0, top_slices[i], 1) # forward run is padded after top_slices
-                _o_backward = torch.narrow(_o[i, : , 1], 0, -1           , 1) # reverse run starts from padded
+                _o_forward  = torch.narrow(o[i, : , 0], 0, top_slices[i], 1) # forward run is padded after top_slices
+                _o_backward = torch.narrow(o[i, : , 1], 0, -1           , 1) # reverse run starts from padded
                 tmp = (_o_forward + _o_backward) / 2.
-                o.append(tmp)
+                _o.append(tmp)
 
             if self.RECORD_ON:
                 self.play_back.extend(self.lstm_rater.get_playback())
                 self.lstm_rater.clean_playback()
 
             if len(o) > 1:
-                o = torch.cat(o)
+                o = torch.cat(_o)
             else:
-                o = o[0]
+                o = _o[0]
             del _o
         else:
             raise AttributeError(f"Got wrong mode: {self._mode}, can only be one of [1|2|3|4|5].")
@@ -230,7 +227,11 @@ class Transformer_rater(nn.Module):
         self.pos_encoder = PositionalEncoding(d_model=in_ch)
 
         self.dropout = nn.Dropout(p=dropout)
-        self.out_fc = nn.Linear(embed_ch, out_ch)
+        self.out_fc = nn.Sequential(
+            nn.LayerNorm(embed_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(embed_ch, out_ch)
+        )
         self.lstm = nn.LSTM(in_ch, embed_ch, bidirectional=True, num_layers=2, batch_first=True)
 
         # for playback
@@ -280,13 +281,13 @@ class Transformer_rater(nn.Module):
 
         # embed with transformer encoder
         # input (B x C x S), but pos_encoding request [S, B, C]
-        embed = self.embedding(self.pos_encoder(x.permute(2, 0, 1)), src_key_padding_mask=padding_mask)
+        # embed = self.embedding(self.pos_encoder(x.permute(2, 0, 1)), src_key_padding_mask=padding_mask)
         # embeded: (S, B, C) -> (B, S, C)
-        embed = embed.permute(1, 0, 2)
+        # embed = embed.permute(1, 0, 2)
 
         # LSTM embed: (B, S, C) -> _o: (B, S, 2 x C) !!LSTM is bidirectional!!
         # !note that LSTM reorders reverse direction run of `output` already
-        _o, (_h, _c) = self.lstm(embed)
+        _o, (_h, _c) = self.lstm(x.permute(0, 2, 1))
 
         play_back = []
 
@@ -330,12 +331,12 @@ class LSTM_rater(nn.Module):
         # Batch size should be 1
         # self.lstm_reviewer = nn.LSTM(in_ch, embeded, batch_first=True, bias=True)
 
-        trans_encoder_layer = nn.TransformerEncoderLayer(d_model=in_ch, nhead=4, dim_feedforward=embed_ch, dropout=dropout)
-        self.embedding = nn.TransformerEncoder(trans_encoder_layer, num_layers=6)
-        self.pos_encoder = PositionalEncoding(d_model=in_ch)
-
         self.dropout = nn.Dropout(p=dropout)
-        self.out_fc = nn.Linear(embed_ch, out_ch)
+        self.out_fc = nn.Sequential(
+            nn.LayerNorm(embed_ch),
+            nn.ReLU(inplace=True),
+            nn.Linear(embed_ch, out_ch)
+        )
         self.lstm = nn.LSTM(in_ch, embed_ch, bidirectional=True, num_layers=2, batch_first=True)
 
         # for playback
@@ -347,15 +348,9 @@ class LSTM_rater(nn.Module):
         self.register_buffer('_mode', torch.IntTensor([1])) # let it save the state too
         self.register_buffer('_embed_ch', torch.IntTensor([embed_ch]))
 
-        # self.init() # initialization
-        # if os.getenv('CUBLAS_WORKSPACE_CONFIG') not in [":16:8", ":4096:2"]:
-        #     raise AttributeError("You are invoking LSTM without properly setting the environment CUBLAS_WORKSPACE_CONFIG"
-        #                          ", which might result in non-deterministic behavior of LSTM. See "
-        #                          "https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html#torch.nn.LSTM for more.")
-
     @property
     def RECORD_ON(self):
-        return self._record_on
+        return self._RECORD_ON
 
     @RECORD_ON.setter
     def RECORD_ON(self, r):
@@ -368,7 +363,7 @@ class LSTM_rater(nn.Module):
             raise ValueError("There are only stage `1` or `2`, when mode = [1|2], this runs in stage 1, when "
                              "mode = [3|4], this runs in stage 2.")
 
-    def forward_(self, x: torch.Tensor, padding_mask: Optional[torch.Tensor] = None):
+    def forward_(self, x: torch.Tensor, seq_length: Union[torch.Tensor, list]):
         r"""
 
 
@@ -384,21 +379,25 @@ class LSTM_rater(nn.Module):
         num_slice = x.shape[-1]
 
         # LSTM (B x C x S) -> (B x S x C)
+        x = x.permute(0, 2, 1)
+        x = pack_padded_sequence(x, seq_length, batch_first=True, enforce_sorted=False)
         # !note that LSTM reorders reverse direction run of `output` already
-        _o, (_h, _c) = self.lstm(x.permute(0, 2, 1))
+        _o, (_h, _c) = self.lstm(x)
+        _o = pad_packed_sequence(_o, batch_first=True)[0] # convert back into tensor
 
         play_back = []
 
         # _o: (B, S, C) -> _o: (B x S x 2 x C)
         bsize, ssize, _ = _o.shape
         o = _o.view(bsize, ssize, 2, -1) # separate the outputs from two direction
-        o = self.out_fc(o)    # _o: (B x S x 2 x fc)
+        o = self.out_fc(self.dropout(o))    # _o: (B x S x 2 x fc)
 
         if self._RECORD_ON:
             # d = direction, s = slice_index
             d = torch.cat([torch.zeros(ssize), torch.ones(ssize)]).view(1, -1, 1)
             slice_index = torch.cat([torch.arange(ssize)] * 2).view(1 ,-1, 1)
-            _o = o.view(bsize, ssize, -1)
+            _o = o.view(bsize, ssize, -1).detach().cpu()
+            _o = torch.cat([_o[..., 0], _o[..., 1]], dim=1).view(bsize, -1, 1)
             row = torch.cat([_o, d.expand_as(_o), slice_index.expand_as(_o)], dim=-1) # concat chans
             self.play_back.append(row)
         return o # no need to deal with up or down afterwards
