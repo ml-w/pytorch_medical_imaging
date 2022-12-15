ba4ca97754baec8c1b88fc5d841156f1ae706d15
alabamagan <fromosia@gmail.com>
2022-12-15 17:33:52 +0800

    Remove old solver

--- pytorch_med_imaging/solvers/__init__.py
+++ pytorch_med_imaging/solvers/__init__.py
@@ -1,7 +1,6 @@
 from .SegmentationSolver import *
 from .ClassificationSolver import *
 from .BinaryClassificationSolver import *
-from .BinaryClassificationRNNSolver_old import *
 from .SurvivalSolver import *
 from .rAIdiologistSolver import *
 from .SolverBase import *
