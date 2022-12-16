bb1e77b576d362d2b008b509814b1322531dcd6d
alabamagan <fromosia@gmail.com>
2022-12-16 20:10:39 +0800

    Clean up code

--- pytorch_med_imaging/solvers/__init__.py
+++ pytorch_med_imaging/solvers/__init__.py
@@ -1,6 +1,4 @@
 from .SegmentationSolver import *
 from .ClassificationSolver import *
 from .BinaryClassificationSolver import *
-from .SurvivalSolver import *
-from .rAIdiologistSolver import *
 from .SolverBase import *
