086ea1120ea7a142422bbe1e254f6b445486853a
alabamagan <fromosia@gmail.com>
2023-01-12 11:39:36 +0800

    Fix typo and replace the logger

--- pytorch_med_imaging/pmi_data_loader/pmi_distributed_data_wrapper.py
+++ pytorch_med_imaging/pmi_data_loader/pmi_distributed_data_wrapper.py
@@ -4,7 +4,7 @@ import torch.multiprocessing as mp
 from torch.utils.data import DistributedSampler
 from . import PMIDataLoaderBase
 from typing import Optional
-
+from mnts.mnts_logger import MNTSLogger
 import torchio as tio
 
 __all__ = ['PMIDistributedDataWrapper']
@@ -17,9 +17,10 @@ class PMIDistributedDataWrapper:
         self.data_loader = data_loader
         self.num_replicas = num_replicas
         self.rank = rank
+        self._logger = MNTSLogger[self.data_loader.__class__.__name__ + f"-{rank}"]
 
     def __getattr__(self, item):
-        return getattr(self, item)
+        return getattr(self.data_loader, item)
 
     def _pack_data_into_subjects(self, *args):
         subjects = self.data_loader._pack_data_into_subjects(*args)
