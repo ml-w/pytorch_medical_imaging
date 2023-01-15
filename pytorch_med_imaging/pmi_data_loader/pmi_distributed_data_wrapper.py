6467c7dea852dadbaa4a46cb691b65707a482073
alabamagan <fromosia@gmail.com>
2023-01-15 16:20:43 +0800

    Temp fix for reloading the data and shuffle it after each epoch

--- pytorch_med_imaging/pmi_data_loader/pmi_distributed_data_wrapper.py
+++ pytorch_med_imaging/pmi_data_loader/pmi_distributed_data_wrapper.py
@@ -6,6 +6,7 @@ from . import PMIDataLoaderBase
 from typing import Optional
 from mnts.mnts_logger import MNTSLogger
 import torchio as tio
+import random
 
 __all__ = ['PMIDistributedDataWrapper']
 
@@ -19,12 +20,37 @@ class PMIDistributedDataWrapper:
         self.rank = rank
         self._logger = MNTSLogger[self.data_loader.__class__.__name__ + f"-DDP-{rank:02d}"]
 
+        _ = [
+            '_pack_data_into_subjects',
+        ]
+        for func_name in _:
+            setattr(self.data_loader, func_name + '_', getattr(self.data_loader, func_name))
+            setattr(self.data_loader, func_name, getattr(self, func_name))
     def __getattr__(self, item):
         return getattr(self.data_loader, item)
 
     def _pack_data_into_subjects(self, *args):
-        subjects = self.data_loader._pack_data_into_subjects(*args)
-        new_subjects = tio.SubjectsDataset(subjects._subjects[self.rank:len(subjects._subjects):self.num_replicas],
+        r"""Each process should handle different set of data. However, if the number of data in each process are
+        different, sync-batch will cause dead lock. Therefore, the length is normalized to always 'drop last'.
+
+        .. warning::
+            Note that this class would not automatically update the batch_size of each subprocesses and it rely on
+            the user to manually reset the batch_size when DDP is activated.
+
+        See Also:
+            * :func:`~pytorch_med_imaging.pmi_data_loader._pack_data_into_subjects`
+        """
+        subjects = self.data_loader._pack_data_into_subjects_(*args)
+
+        random_seed = random.randint(0, 1E16)
+        random_seed = torch.as_tensor([random_seed], dtype=torch.int32).cuda(device=dist.get_rank())
+        dist.broadcast(random_seed, 0) # broadcast 0-th rank random_seed
+        random.Random(random_seed.item()).shuffle(subjects._subjects) # use the same random seed to ensure the shuffled
+                                                                      # order is the same
+
+        _len = len(subjects._subjects) - len(subjects._subjects) % self.num_replicas
+        new_subjects = tio.SubjectsDataset(subjects._subjects[self.rank:_len:self.num_replicas],
                                            transform = subjects._transform)
+        del random_seed
         return new_subjects
 
