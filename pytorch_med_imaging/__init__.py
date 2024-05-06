8e9c7ff7285d5bb62e66b36a6d8b1ee619448acb
lwong <fromosia@gmail.com>
2024-05-06 17:47:56 +0800

    Fix import bug

--- pytorch_med_imaging/__init__.py
+++ pytorch_med_imaging/__init__.py
@@ -1,10 +1,2 @@
-from . import med_img_dataset
-from . import pmi_data_loader
-from . import loss
-from . import networks
-from . import inferencers
-from . import solvers
-from . import utils
-
 # __all__ = ['MedImgDataset', 'PMIDataLoader', 'Loss', 'Networks', 'Inferencers', 'Solvers']
 # __all__ = ['loss', 'med_img_dataset', 'pmi_data_loader', 'networks', 'inferencers']
