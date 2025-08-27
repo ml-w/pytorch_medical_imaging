ce39a0e8e7c374a2c974ccebfc7122452cf9f15a
lwong <fromosia@gmail.com>
2025-08-27 22:49:50 +0800

    Update setup script

--- setup.py
+++ setup.py
@@ -1,37 +1,28 @@
-# python setup.py build_ext --inplace
-import numpy
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
+import numpy
 
-
-cmdclass = {}
-ext_modules = []
-if use_cython:
-    ext_modules += [
-        Extension("pytorch_med_imaging.pmi_data.computations._LocalNeighborhoodDifferencePattern",
+extensions = [
+    Extension(
+        "pytorch_med_imaging.pmi_data.computations._LocalNeighborhoodDifferencePattern",
                   ["pytorch_med_imaging/pmi_data/computations/_LocalNeighborhoodDifferencePattern.pyx"],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    ext_modules += [
-        Extension("pytorch_med_imaging.pmi_data.computations._interpolation",
+        include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+        "pytorch_med_imaging.pmi_data.computations._interpolation",
                   ["pytorch_med_imaging/pmi_data/computations/_interpolation.pyx"],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    ext_modules += [
-        Extension("pytorch_med_imaging.pmi_data.computations._prob_func",
+        include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+        "pytorch_med_imaging.pmi_data.computations._prob_func",
                   ["pytorch_med_imaging/pmi_data/computations/_prob_func.pyx"],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    cmdclass.update({'build_ext': build_ext})
-
+        include_dirs=[numpy.get_include()],
+    ),
+]
 setup(
-    ext_modules=ext_modules,
-    cmdclass=cmdclass
+    name="pytorch-med-imaging",
+    version="1.0",
+    packages=["pytorch_med_imaging.pmi_data.computations"],
+    ext_modules=cythonize(extensions, language_level="3"),
 )
+
